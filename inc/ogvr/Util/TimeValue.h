b0011df56af22ceaff4e9fa45bb3b1be444fd7d3
Ryan Pavlik <ryan@sensics.com>
2014-10-24 15:07:39 -0500

    Add two header stubs.

---
+++ inc/ogvr/Util/TimeValue.h
@@ -0,0 +1,40 @@
+/** @file
+    @brief Header
+
+    @date 2014
+
+    @author
+    Ryan Pavlik
+    <ryan@sensics.com>;
+    <http://sensics.com>
+*/
+
+// Copyright 2014 Sensics, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef INCLUDED_TimeValue_h_GUID_AD9F3D81_382D_4394_433B_A8026BE803B6
+#define INCLUDED_TimeValue_h_GUID_AD9F3D81_382D_4394_433B_A8026BE803B6
+
+
+// Internal Includes
+// - none
+
+// Library/third-party includes
+// - none
+
+// Standard includes
+// - none
+
+#endif // INCLUDED_TimeValue_h_GUID_AD9F3D81_382D_4394_433B_A8026BE803B6
+
