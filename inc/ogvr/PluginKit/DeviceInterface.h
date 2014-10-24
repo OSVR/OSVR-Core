cb1dd98dfe9973005f90c2c5da3ba0628735e3db
Ryan Pavlik <ryan@sensics.com>
2014-10-24 14:51:41 -0500

    Add new file.

---
+++ inc/ogvr/PluginKit/DeviceInterface.h
@@ -0,0 +1,41 @@
+/** @file
+    @brief Header
+
+    @date 2014
+
+    @author
+    Ryan Pavlik
+    <ryan@sensics.com>;
+    <http://sensics.com>
+*/
+
+// Copyright 2014 Sensics, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef INCLUDED_DeviceInterface_h_GUID_A929799C_02F5_4C92_C503_36C7F59D6BA1
+#define INCLUDED_DeviceInterface_h_GUID_A929799C_02F5_4C92_C503_36C7F59D6BA1
+
+// Internal Includes
+// - none
+
+// Library/third-party includes
+// - none
+
+// Standard includes
+// - none
+namespace ogvr {
+namespace device {} // end of namespace device
+} // end of namespace ogvr
+
+#endif // INCLUDED_DeviceInterface_h_GUID_A929799C_02F5_4C92_C503_36C7F59D6BA1
