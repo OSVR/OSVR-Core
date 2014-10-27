b5eefaa4d86059540929f04fae8302b8daa2cef5
Ryan Pavlik <ryan@sensics.com>
2014-10-27 17:37:38 -0500

    update cibuild script
    Hopefully enabling windows ci build.

--- devtools/cibuild.cmd
+++ devtools/cibuild.cmd
@@ -1,12 +1,13 @@
 cd /d "%~dp0"
 cd ..
+rmdir /s /q install
 rmdir /s /q build
 mkdir build
 cd build
-cmake .. -G "Visual Studio 12"
+cmake .. -G "Visual Studio 12" "-DCMAKE_INSTALL_PREFIX=%WORKSPACE%\install" %*
 cmake --build . --config Debug
 cmake --build . --config RelWithDebInfo
-cmake --build . --config Debug --target doc
+cmake --build . --config RelWithDebInfo --target INSTALL
 
 rem bin\Debug\LoadTest.exe --gtest_output=xml:test_details.Debug.xml
 rem bin\RelWithDebInfo\LoadTest.exe --gtest_output=xml:test_details.RelWithDebInfo.xml
