8a37921eca2ab309c4cc8b3011b4d6cce6218798
Ryan Pavlik <ryan@sensics.com>
2014-10-30 16:46:49 -0500

    Add a self-contained example plugin.

---
+++ examples/selfcontained/org_opengoggles_example_selfcontainedDummyAsync.cpp
@@ -0,0 +1,76 @@
+/** @file
+    @brief Implementation
+
+    @date 2014
+
+    @author
+    Ryan Pavlik
+    <ryan@sensics.com>;
+    <http://sensics.com>
+*/
+
+// Copyright 2014 Sensics, Inc.
+//
+// All rights reserved.
+//
+// (Final version intended to be licensed under
+// the Apache License, Version 2.0)
+
+// Internal Includes
+#include <ogvr/PluginKit/PluginInterfaceC.h>
+#include <ogvr/Util/GenericDeleter.h>
+
+// Library/third-party includes
+// - none
+
+// Standard includes
+#include <iostream>
+
+namespace {
+class DummyAsyncDevice {
+  public:
+    DummyAsyncDevice(OGVR_DeviceToken d) : m_dev(d) {
+        std::cout << "Constructing dummy asynchronous (threaded) device"
+                  << std::endl;
+    }
+    /// Another trampoline.
+    ///
+    /// In this case, the core spawns a thread, with a loop calling this
+    /// function
+    /// as long as things are running. So this function waits for the next
+    /// message from the device and passes it on.
+    static OGVR_PluginReturnCode wait(void *userData) {
+        return static_cast<DummyAsyncDevice *>(userData)->m_wait();
+    }
+    ~DummyAsyncDevice() {
+        std::cout << "Destroying dummy asynchronous (threaded) device"
+                  << std::endl;
+    }
+
+  private:
+    OGVR_PluginReturnCode m_wait() {
+        // block on waiting for data.
+        // once we have enough, call
+        char *mydata = NULL;
+        ogvrDeviceSendData(m_dev, mydata, 0);
+        return OGVR_PLUGIN_SUCCESS;
+    }
+    OGVR_DeviceToken m_dev;
+};
+}
+
+OGVR_PLUGIN(org_opengoggles_example_selfcontainedDummyAsync) {
+    /// Create an asynchronous (threaded) device
+    OGVR_DeviceToken d;
+    ogvrDeviceAsyncInit(ctx, "MyAsyncDevice",
+                        &d); // Puts an object in d that knows it's a
+                             // threaded device so ogvrDeviceSendData knows
+                             // that it needs to get a connection lock first.
+    DummyAsyncDevice *myAsync = new DummyAsyncDevice(d);
+    ogvrPluginRegisterDataWithDeleteCallback(
+        ctx, &ogvr::detail::generic_deleter<DummyAsyncDevice>,
+        static_cast<void *>(myAsync));
+    ogvrDeviceAsyncStartWaitLoop(d, &DummyAsyncDevice::wait,
+                                 static_cast<void *>(myAsync));
+    return OGVR_PLUGIN_SUCCESS;
+}
