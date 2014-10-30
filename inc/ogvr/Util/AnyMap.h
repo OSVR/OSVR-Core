4ec10bf26b255b383145f583a7fd1ca64be0c10e
Ryan Pavlik <ryan@sensics.com>
2014-10-30 10:02:02 -0500

    Make the AnyMap an actual class so we have easier methods and some implementation hiding.
    The actual interface of std::map isn't super user-friendly and
    we might want to switch to unordered_map, etc. at some point.

--- inc/ogvr/Util/AnyMap.h
+++ inc/ogvr/Util/AnyMap.h
@@ -20,7 +20,7 @@
 #define INCLUDED_AnyMap_h_GUID_794993EB_B778_4E88_16A7_3A047A615587
 
 // Internal Includes
-// - none
+#include <ogvr/Util/Export.h>
 
 // Library/third-party includes
 #include <boost/any.hpp>
@@ -31,7 +31,48 @@
 
 namespace ogvr {
 /// @brief A data structure storing "any" by name, to reduce coupling.
-typedef std::map<std::string, boost::any> AnyMap;
+class AnyMap {
+  public:
+    /// @brief Do we have data under this key?
+    OGVR_UTIL_EXPORT bool contains(std::string const &key) const;
+    /// @overload
+    OGVR_UTIL_EXPORT bool contains(const char *key) const;
+
+    /// @brief Set data for the given key
+    ///
+    /// Silently overwrites existing data at that key.
+    /// @{
+    OGVR_UTIL_EXPORT void set(std::string const &key, boost::any const &value);
+    OGVR_UTIL_EXPORT void set(const char *key, boost::any const &value);
+
+    template <typename T> void set(std::string const &key, T value) {
+        set(key, boost::any(value));
+    }
+
+    template <typename T> void set(const char *key, T value) {
+        set(key, boost::any(value));
+    }
+    /// @}
+
+    /// @brief Get the data for this key.
+    ///
+    /// Returns an empty boost::any if the key doesn't exist.
+    OGVR_UTIL_EXPORT boost::any get(std::string const &key);
+    /// @overload
+    OGVR_UTIL_EXPORT boost::any get(const char *key);
+
+    /// @brief Clears the data for this key.
+    ///
+    /// If the key doesn't exist, this method does nothing
+    OGVR_UTIL_EXPORT void erase(std::string const &key);
+    /// @overload
+    OGVR_UTIL_EXPORT void erase(const char *key);
+
+  private:
+    typedef std::map<std::string, boost::any> Contents;
+    Contents m_contents;
+};
+
 } // end of namespace ogvr
 
 #endif // INCLUDED_AnyMap_h_GUID_794993EB_B778_4E88_16A7_3A047A615587
