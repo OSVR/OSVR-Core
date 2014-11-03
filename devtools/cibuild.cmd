819e8b30b807aee7a7d0f504ea66a8c905042284
Ryan Pavlik <ryan@sensics.com>
2014-11-03 17:40:32 -0600

    Change build order for CI.

--- devtools/cibuild.cmd
+++ devtools/cibuild.cmd
@@ -7,8 +7,9 @@ cd build
 cmake .. -G "Visual Studio 12" "-DCMAKE_INSTALL_PREFIX=%WORKSPACE%\install" %*
 cmake --build . --config Debug
 cmake --build . --config RelWithDebInfo
-cmake --build . --config RelWithDebInfo -- /p:RunCodeAnalysis=True
 cmake --build . --config RelWithDebInfo --target INSTALL
+
+cmake --build . --config RelWithDebInfo -- /p:RunCodeAnalysis=True
 
 rem bin\Debug\LoadTest.exe --gtest_output=xml:test_details.Debug.xml
 rem bin\RelWithDebInfo\LoadTest.exe --gtest_output=xml:test_details.RelWithDebInfo.xml
