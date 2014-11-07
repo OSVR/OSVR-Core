bcd428f1f813869ad423f390162d4799c0bb6da3
Ryan Pavlik <ryan@sensics.com>
2014-11-07 13:38:08 -0600

    Add an example plugin using the C++ stuff.

---
+++ examples/plugin/org_opengoggles_example_DummyHardwarePollCpp.cpp
@@ -0,0 +1,56 @@
+/** @file
+    @brief Implementation
+
+    @date 2014
+
+    @author
+    Ryan Pavlik
+    <ryan@sensics.com>;
+    <http://sensics.com>
+*/
+
+// Copyright 2014 Sensics, Inc.
+//
+// All rights reserved.
+//
+// (Final version intended to be licensed under
+// the Apache License, Version 2.0)
+
+// Internal Includes
+#include <ogvr/PluginKit/PluginRegistration.h>
+
+// Library/third-party includes
+// - none
+
+// Standard includes
+#include <iostream>
+
+class HardwarePoller {
+  public:
+    HardwarePoller(int initialVal) : m_data(initialVal) {}
+    OGVR_ReturnCode operator()(OGVR_PluginRegContext /*ctx*/) {
+
+        std::cout << "Got a poll for hardware change, with dummy data "
+                  << m_data << std::endl;
+        m_data = 1;
+        std::cout << "Data now " << m_data << std::endl;
+        return OGVR_RETURN_SUCCESS;
+    }
+
+  private:
+    /// @brief Some dummy example data.
+    int m_data;
+};
+
+OGVR_PLUGIN(org_opengoggles_example_DummyHardwarePoll) {
+    /// Register a polling callback function object: here passing it result of
+    /// "new".
+    /// Auto-registers for destruction.
+    ogvr::plugin::registerHardwarePollCallback(ctx, new HardwarePoller(0));
+
+    /// Register a polling callback function object: here passing it a value.
+    /// It will make a copy that it auto-registers for destruction.
+    ogvr::plugin::registerHardwarePollCallback(ctx, HardwarePoller(10));
+
+    return OGVR_RETURN_SUCCESS;
+}
