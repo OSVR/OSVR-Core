7f446a547f7761a8512f72be84af1fb3bd426579
Ryan Pavlik <ryan@sensics.com>
2014-11-10 14:16:27 -0600

    Add stdint.h wrapper.

---
+++ inc/ogvr/Util/StdInt.h
@@ -0,0 +1,30 @@
+/** @file
+    @brief Header wrapping the <stdint.h> header.
+
+    Must be c-safe!
+
+    @date 2014
+
+    @author
+    Ryan Pavlik
+    <ryan@sensics.com>
+    <http://sensics.com>
+*/
+
+/*
+// Copyright 2014 Sensics, Inc.
+//
+// All rights reserved.
+//
+// (Final version intended to be licensed under
+// the Apache License, Version 2.0)
+*/
+
+#ifndef INCLUDED_StdInt_h_GUID_C1AAF35C_C704_4DB7_14AC_615730C4619B
+#define INCLUDED_StdInt_h_GUID_C1AAF35C_C704_4DB7_14AC_615730C4619B
+
+#if !defined(_MSC_VER) || (defined(_MSC_VER) && _MSC_VER > 1600)
+#include <stdint.h>
+#endif
+
+#endif
