7de0867cb69f5293bc337abd1c13973da2faff65
Ryan Pavlik <ryan@sensics.com>
2014-11-10 13:20:34 -0600

    Stub in client library

---
+++ inc/ogvr/Client/ClientC.h
@@ -0,0 +1,35 @@
+/** @file
+    @brief Header
+
+    Must be c-safe!
+
+    @date 2014
+
+    @author
+    Ryan Pavlik
+    <ryan@sensics.com>
+    <http://sensics.com>
+*/
+
+/*
+// Copyright 2014 Sensics, Inc.
+//
+// All rights reserved.
+//
+// (Final version intended to be licensed under
+// the Apache License, Version 2.0)
+*/
+
+#ifndef INCLUDED_ClientC_h_GUID_CB5C5F9B_08F8_4B8E_9E7C_BE706762E6FB
+#define INCLUDED_ClientC_h_GUID_CB5C5F9B_08F8_4B8E_9E7C_BE706762E6FB
+
+/* Internal Includes */
+/* none */
+
+/* Library/third-party includes */
+/* none */
+
+/* Standard includes */
+/* none */
+
+#endif
