6ce60ce161bc76fa9b8bf02e2de9ead1d3372d61
Ryan Pavlik <ryan@sensics.com>
2014-11-10 16:16:48 -0600

    Add VRPN directory

---
+++ inc/ogvr/VRPN/ConstructVRPNObject.h
@@ -0,0 +1,31 @@
+/** @file
+    @brief Header
+
+    @date 2014
+
+    @author
+    Ryan Pavlik
+    <ryan@sensics.com>
+    <http://sensics.com>
+*/
+
+// Copyright 2014 Sensics, Inc.
+//
+// All rights reserved.
+//
+// (Final version intended to be licensed under
+// the Apache License, Version 2.0)
+
+#ifndef INCLUDED_ConstructVRPNObject_h_GUID_C8AAF17C_5713_42BB_18A6_4B2291BB05B1
+#define INCLUDED_ConstructVRPNObject_h_GUID_C8AAF17C_5713_42BB_18A6_4B2291BB05B1
+
+// Internal Includes
+// - none
+
+// Library/third-party includes
+// - none
+
+// Standard includes
+// - none
+
+#endif // INCLUDED_ConstructVRPNObject_h_GUID_C8AAF17C_5713_42BB_18A6_4B2291BB05B1
