8dab05a053d64af531b75be7c48b795de9ed6152
Ryan Pavlik <ryan@sensics.com>
2014-11-11 17:24:00 -0600

    Some implementation for the Multiserver.
    Get a crash on shutdown (in shared pointer deleter), probably
    related to allocating a derived class in a plugin that inherits
    from a base class in another DLL.

--- plugins/multiserver/org_opengoggles_bundled_Multiserver.cpp
+++ plugins/multiserver/org_opengoggles_bundled_Multiserver.cpp
@@ -18,7 +18,11 @@
 // the Apache License, Version 2.0)
 
 // Internal Includes
+#include "GetVRPNConnection.h"
 #include <ogvr/PluginKit/PluginKit.h>
+#include <ogvr/PluginHost/RegistrationContext.h>
+#include <ogvr/PluginHost/PluginSpecificRegistrationContext.h>
+#include <ogvr/Connection/Connection.h>
 #include <ogvr/Connection/ConnectionDevice.h>
 #include <ogvr/Util/UniquePtr.h>
 
@@ -31,6 +35,7 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <sstream>
 
 template <typename T>
 class VRPNCustomConnectionDevice : public ogvr::connection::ConnectionDevice {
@@ -38,6 +43,7 @@ class VRPNCustomConnectionDevice : public ogvr::connection::ConnectionDevice {
     VRPNCustomConnectionDevice(std::string const &name, vrpn_Connection *conn)
         : ogvr::connection::ConnectionDevice(name),
           m_dev(new T(name.c_str(), conn)) {}
+    virtual ~VRPNCustomConnectionDevice() {}
     virtual void m_process() { m_dev->mainloop(); }
     virtual void m_sendData(ogvr::util::time::TimeValue const &,
                             ogvr::connection::MessageType *, const char *,
@@ -53,8 +59,41 @@ class VRPNHardwarePoll {
   public:
     typedef std::map<std::string, size_t> NameCountMap;
     OGVR_ReturnCode operator()(OGVR_PluginRegContext ctx) {
-        hid_enumerate(0, 0);
+        bool found = false;
+        for (struct hid_device_info *dev = hid_enumerate(0, 0); dev != NULL;
+             dev = dev->next) {
+            if (found) {
+                continue;
+            }
+            if (dev->vendor_id == 0x1532 && dev->product_id == 0x0300) {
+                // Razer Hydra
+                found = true;
+                constructAndRegister<vrpn_Tracker_RazerHydra>(ctx,
+                                                              "RazerHydra");
+            }
+        }
         return OGVR_RETURN_SUCCESS;
+    }
+    template <typename T>
+    void constructAndRegister(OGVR_PluginRegContext ctx,
+                              std::string const &nameStem) {
+        ogvr::pluginhost::PluginSpecificRegistrationContext &pluginCtx =
+            ogvr::pluginhost::PluginSpecificRegistrationContext::get(ctx);
+        std::string name = pluginCtx.getName() + "/" + assignName(nameStem);
+        std::cout << "Creating " << name << std::endl;
+        ogvr::connection::ConnectionPtr conn =
+            ogvr::connection::Connection::retrieveConnection(
+                pluginCtx.getParent());
+        ogvr::connection::ConnectionDevicePtr dev(
+            new VRPNCustomConnectionDevice<T>(name, getVRPNConnection(ctx)));
+
+        conn->addDevice(dev);
+    }
+    std::string assignName(std::string const &nameStem) {
+        size_t num = assignNumber(nameStem);
+        std::ostringstream os;
+        os << nameStem << num;
+        return os.str();
     }
     size_t assignNumber(std::string const &nameStem) {
         NameCountMap::iterator it = m_nameCount.find(nameStem);
