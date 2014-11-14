abc76834499493424b6cc6d0020c89c85ad9147b
Ryan Pavlik <ryan@sensics.com>
2014-11-14 12:27:53 -0600

    Call the testing batch files, don't just go there permanently.

--- devtools/cibuild.cmd
+++ devtools/cibuild.cmd
@@ -8,8 +8,8 @@ cmake .. -G "Visual Studio 12" "-DCMAKE_INSTALL_PREFIX=%WORKSPACE%\install" %*
 cmake --build . --config Debug
 cmake --build . --config RelWithDebInfo
 
-"%WORKSPACE%\vendor\jenkins-ctest-plugin\run-test-and-save.bat" -C Debug
-"%WORKSPACE%\vendor\jenkins-ctest-plugin\run-test-and-save.bat" -C RelWithDebInfo
+call "%WORKSPACE%\vendor\jenkins-ctest-plugin\run-test-and-save.bat" -C Debug
+call "%WORKSPACE%\vendor\jenkins-ctest-plugin\run-test-and-save.bat" -C RelWithDebInfo
 
 cmake --build . --config Debug --target INSTALL
 cmake --build . --config RelWithDebInfo --target INSTALL
