7b365a014795654e927745b491771d6af2e45759
Ryan Pavlik <ryan@sensics.com>
2014-11-21 11:21:20 -0600

    More testing work.

--- tests/cplusplus/Routing/PathTree.cpp
+++ tests/cplusplus/Routing/PathTree.cpp
@@ -35,14 +35,34 @@ using namespace osvr::routing;
 
 TEST(PathTree, create) { ASSERT_NO_THROW(PathTree()); }
 
+TEST(PathElement, getTypeName) {
+    ASSERT_STREQ(elements::getTypeName<elements::NullElement>(), "NullElement");
+}
+
 TEST(PathTree, getPath) {
     PathTree tree;
-    ASSERT_NO_THROW(tree.getNodeByPath("/test"));
-    ASSERT_STREQ(getTypeName(tree.getNodeByPath("/test")), "NullElement");
-    ASSERT_STREQ(elements::getTypeName<elements::NullElement>(), "NullElement");
-    ASSERT_EQ(tree.getNodeByPath("/test").getName(), "test");
+    ASSERT_NO_THROW(tree.getNodeByPath("/")) << "Get the root.";
 
-    PathNode *result = NULL;
-    ASSERT_NO_THROW(result = &tree.getNodeByPath("/test1/test2"));
-    ASSERT_EQ(result->getName(), "test2");
+    {
+        PathNode *result = NULL;
+        ASSERT_NO_THROW(result = &tree.getNodeByPath("/test"))
+            << "Get a new node just a single level in";
+        ASSERT_STREQ(getTypeName(*result), "NullElement")
+            << "Check the type of the new node.";
+        ASSERT_EQ(result->getName(), "test")
+            << "Check the name of the new node";
+
+        ASSERT_NO_THROW(tree.getNodeByPath("/test"))
+            << "Get the same node again.";
+        ASSERT_EQ(tree.getNodeByPath("/test"), *result)
+            << "Ensure we're actually getting the same identity.";
+    }
+    {
+        PathNode *test2 = NULL;
+        ASSERT_NO_THROW(test2 = &tree.getNodeByPath("/test1/test2"))
+            << "Get a node two new levels in";
+        ASSERT_EQ(test2->getName(), "test2");
+        ASSERT_TRUE(test2->getParent().get());
+        PathNodePtr test1 = test2->getParent();
+    }
 }
