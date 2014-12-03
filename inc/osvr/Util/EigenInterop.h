741387c7f562499488a8549e040a4849100ad285
Ryan Pavlik <ryan@sensics.com>
2014-12-02 18:26:37 -0600

    Implement Eigen Interop.

--- inc/osvr/Util/EigenInterop.h
+++ inc/osvr/Util/EigenInterop.h
@@ -31,6 +31,28 @@
 // - none
 
 namespace osvr {
-namespace util {} // namespace util
+namespace util {
+    inline Eigen::Map<Eigen::Vector3d> vecMap(OSVR_Vec3 &vec) {
+        return Eigen::Map<Eigen::Vector3d>(vec.data);
+    }
+
+    inline Eigen::Map<const Eigen::Vector3d> vecMap(OSVR_Vec3 const &vec) {
+        return Eigen::Map<const Eigen::Vector3d>(vec.data);
+    }
+
+    inline Eigen::Isometry3d fromPose(OSVR_Pose3 const &pose) {
+        Eigen::Map<const Eigen::Vector3d> xlateVec(pose.translation.data);
+        return Eigen::Translation3d(vecMap(pose.translation)) *
+               Eigen::Quaterniond(pose.rotation.data);
+    }
+
+    inline void toPose(Eigen::Isometry3d const &xform, OSVR_Pose3 &pose) {
+        Eigen::Vector3d::Map(pose.translation.data) = xform.translation();
+        Eigen::Quaterniond quat(xform.rotation());
+        Eigen::Vector3d::Map(&pose.rotation.data[1]) = quat.vec();
+        pose.rotation.data[0] = quat.w();
+    }
+
+} // namespace util
 } // namespace osvr
 #endif // INCLUDED_EigenInterop_h_GUID_A07DFDC3_BA71_4894_6593_732B364A2AE7
