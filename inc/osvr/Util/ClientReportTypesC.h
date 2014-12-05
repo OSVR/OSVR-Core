d832709bdcc5112a1cfae5f79b3ae2a2a3eec0b3
Ryan Pavlik <ryan@sensics.com>
2014-12-05 16:49:37 -0600

    Alternate way of generating for each report type.

---
+++ inc/osvr/Util/ClientReportTypesC.h
@@ -0,0 +1,96 @@
+/** @file
+    @brief Header
+
+    Must be c-safe!
+
+    @date 2014
+
+    @author
+    Ryan Pavlik
+    <ryan@sensics.com>
+    <http://sensics.com>
+*/
+
+/*
+// Copyright 2014 Sensics, Inc.
+//
+// All rights reserved.
+//
+// (Final version intended to be licensed under
+// the Apache License, Version 2.0)
+*/
+
+#ifndef INCLUDED_ClientReportTypesC_h_GUID_E79DAB07_78B7_4795_1EB9_CA6EEB274AEE
+#define INCLUDED_ClientReportTypesC_h_GUID_E79DAB07_78B7_4795_1EB9_CA6EEB274AEE
+
+/* Internal Includes */
+#include <osvr/Util/APIBaseC.h>
+#include <osvr/Util/Pose3C.h>
+#include <osvr/Util/StdInt.h>
+
+/* Library/third-party includes */
+/* none */
+
+/* Standard includes */
+/* none */
+
+OSVR_EXTERN_C_BEGIN
+
+/** @addtogroup ClientKit
+    @{
+*/
+
+/** @brief Report type for a position callback on a tracker interface */
+struct OSVR_PositionReport {
+    /** @brief Identifies the sensor that the report comes from */
+    int32_t sensor;
+    /** @brief The position vector */
+    struct OSVR_Vec3 xyz;
+};
+
+/** @brief Report type for an orientation callback on a tracker interface */
+struct OSVR_OrientationReport {
+    /** @brief Identifies the sensor that the report comes from */
+    int32_t sensor;
+    /** @brief The rotation unit quaternion */
+    struct OSVR_Quaternion rotation;
+};
+
+/** @brief Report type for a pose (position and orientation) callback on a
+    tracker interface
+*/
+struct OSVR_PoseReport {
+    /** @brief Identifies the sensor that the report comes from */
+    int32_t sensor;
+    /** @brief The pose structure, containing a position vector and a rotation
+        quaternion
+    */
+    struct OSVR_Pose3 pose;
+};
+
+/** @brief Type of button state */
+typedef uint8_t OSVR_ButtonState;
+
+/** @brief Report type for a callback on a button interface */
+struct OSVR_ButtonReport {
+    /** @brief Identifies the sensor that the report comes from */
+    int32_t sensor;
+    /** @brief The button state: 1 is pressed, 0 is not pressed. */
+    OSVR_ButtonState state;
+};
+
+/** @brief Type of analog channel state */
+typedef double OSVR_AnalogState;
+
+/** @brief Report type for a callback on an analog interface */
+struct OSVR_AnalogReport {
+    /** @brief Identifies the sensor/channel that the report comes from */
+    int32_t sensor;
+    /** @brief The analog state. */
+    OSVR_AnalogState state;
+};
+
+/** @} */
+OSVR_EXTERN_C_END
+
+#endif
---
+++ src/osvr/GenerateForReportTypes.cmake
@@ -0,0 +1,23 @@
+set(REPORT_TYPES
+    Position
+    Orientation
+    Pose
+    Button
+    Analog)
+
+# Generate a file using a template with the placeholder @BODY@, as well as a
+# repeated template with the placeholder @TYPE@ - repeated once for each report type.
+function(osvr_generate_for_report_types _fileTemplate _snippetTemplate _outFile)
+    message(STATUS "Generating ${_outFile}")
+
+    # This line forces a CMake re-run if the snippet template is modified.
+    configure_file("${_snippetTemplate}" "${_outFile}.snippettemplate" @ONLY)
+
+    file(READ "${_snippetTemplate}" SNIPPET)
+    set(BODY "/* generated file - do not edit! */")
+    foreach(TYPE ${REPORT_TYPES})
+        string(CONFIGURE "${SNIPPET}" snippetConfigured @ONLY)
+        set(BODY "${BODY}\n${snippetConfigured}")
+    endforeach()
+    configure_file("${_fileTemplate}" "${_outFile}" @ONLY)
+endfunction()
