0e766dbdb552b446cfdf4384d5271ae63605578b
Kevin M. Godby <kevin@godby.org>
2014-12-06 12:37:17 -0600

    Fall back on boost::shared_ptr is std::shared_ptr is unavailable.

--- inc/osvr/Util/SharedPtr.h
+++ inc/osvr/Util/SharedPtr.h
@@ -20,10 +20,23 @@
 #ifndef INCLUDED_SharedPtr_h_GUID_E9C5BC8D_7D3A_4896_1552_6F4F5292783C
 #define INCLUDED_SharedPtr_h_GUID_E9C5BC8D_7D3A_4896_1552_6F4F5292783C
 
-#if defined(_MSC_VER) && (_MSC_VER < 1600)
-#error "Not supported before VS 2010"
+#if (__cplusplus <= 199711L) || (defined(_MSC_VER) && (_MSC_VER < 1600))
+// Use Boost if we're not compiling against C++11 or above
+// or if we're using Microsoft Visual Studio < 2010.
+#include <boost/shared_ptr.hpp>
+#include <boost/weak_ptr.hpp>
+#include <boost/make_shared.hpp>
+#include <boost/enable_shared_from_this.hpp>
+
+namespace osvr {
+using boost::shared_ptr;
+using boost::weak_ptr;
+using boost::make_shared;
+using boost::enable_shared_from_this;
+} // end namespace osvr
 
 #else
+// If we're compiling under C++11 or above, use std:: variants.
 #include <memory>
 
 namespace osvr {
