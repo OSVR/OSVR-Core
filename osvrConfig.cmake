887aa34104c66a708ddc6dc830bcc376999f1608
Ryan Pavlik <ryan@sensics.com>
2014-12-08 10:14:17 -0600

    Don't add Boost to the installed interface include dirs for header-only stuff.
    Instead, we search for it in the config file.
    
    Closes #6.

--- osvrConfig.cmake
+++ osvrConfig.cmake
@@ -1,6 +1,10 @@
 find_package(libfunctionality)
+find_package(Boost QUIET)
 
 include("${CMAKE_CURRENT_LIST_DIR}/osvrTargets.cmake")
+target_link_libraries(osvr::osvrUtilCpp INTERFACE ${Boost_INCLUDE_DIRS})
+target_link_libraries(osvr::osvrClientKitCpp INTERFACE ${Boost_INCLUDE_DIRS})
+target_link_libraries(osvr::osvrPluginKitCpp INTERFACE ${Boost_INCLUDE_DIRS})
 
 function(osvr_add_plugin NAME)
     add_library(${NAME} MODULE ${ARGN})
