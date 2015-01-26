1c2ddac4380c5885993d5191404d87dc1047268a
Ryan Pavlik <ryan@sensics.com>
2015-01-26 14:30:14 -0600

    Only look for Boost again in installed builds, and improve out of tree plugin building.

--- osvrConfig.cmake
+++ osvrConfig.cmake
@@ -1,13 +1,16 @@
 find_package(libfunctionality)
-find_package(Boost QUIET)
 
 include("${CMAKE_CURRENT_LIST_DIR}/osvrTargets.cmake")
-set_property(TARGET osvr::osvrUtilCpp APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${Boost_INCLUDE_DIRS})
-set_property(TARGET osvr::osvrClientKitCpp APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${Boost_INCLUDE_DIRS})
-set_property(TARGET osvr::osvrPluginKitCpp APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${Boost_INCLUDE_DIRS})
+
+include("${CMAKE_CURRENT_LIST_DIR}/osvrConfigInstalledBoost.cmake" OPTIONAL)
 
 function(osvr_add_plugin NAME)
     add_library(${NAME} MODULE ${ARGN})
     target_link_libraries(${NAME} osvr::osvrPluginKit)
-    set_target_properties(${NAME} PROPERTIES PREFIX "")
+    set_target_properties(${NAME} PROPERTIES
+        PREFIX ""
+        RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/@OSVR_PLUGIN_DIR@"
+        LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/@OSVR_PLUGIN_DIR@")
+    install(TARGETS ${NAME}
+        LIBRARY DESTINATION @OSVR_PLUGIN_DIR@ COMPONENT Runtime)
 endfunction()
