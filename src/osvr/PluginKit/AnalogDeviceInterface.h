22b85b94d9430cd9ca36d92d7032ac77f23a88b5
Ryan Pavlik <ryan@sensics.com>
2015-01-29 09:08:41 -0600

    Stub files.

---
+++ src/osvr/PluginKit/AnalogDeviceInterface.h
@@ -0,0 +1,30 @@
+/** @file
+    @brief Header
+
+    @date 2015
+
+    @author
+    Sensics, Inc.
+    <http://sensics.com/osvr>
+*/
+
+// Copyright 2015 Sensics, Inc.
+//
+// All rights reserved.
+//
+// (Final version intended to be licensed under
+// the Apache License, Version 2.0)
+
+#ifndef INCLUDED_AnalogDeviceInterface_h_GUID_D262D2C3_9AF6_4286_064A_2C9B73FDC96B
+#define INCLUDED_AnalogDeviceInterface_h_GUID_D262D2C3_9AF6_4286_064A_2C9B73FDC96B
+
+// Internal Includes
+// - none
+
+// Library/third-party includes
+// - none
+
+// Standard includes
+// - none
+
+#endif // INCLUDED_AnalogDeviceInterface_h_GUID_D262D2C3_9AF6_4286_064A_2C9B73FDC96B
