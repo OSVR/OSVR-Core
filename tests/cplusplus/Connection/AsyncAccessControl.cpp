343d4b5d5357c31748eb37c65bb6ac795dfa84e2
Ryan Pavlik <ryan@sensics.com>
2015-01-29 13:21:41 -0600

    Add some async access control tests.

---
+++ tests/cplusplus/Connection/AsyncAccessControl.cpp
@@ -0,0 +1,101 @@
+/** @file
+    @brief Test Implementation
+
+    @date 2014
+
+    @author
+    Sensics, Inc.
+    <http://sensics.com/osvr>
+
+*/
+
+// Copyright 2014 Sensics, Inc.
+//
+// All rights reserved.
+//
+// (Final version intended to be licensed under
+// the Apache License, Version 2.0)
+
+// Internal Includes
+#include "../../../src/osvr/Connection/AsyncAccessControl.h"
+#include "../../../src/osvr/Connection/AsyncAccessControl.cpp"
+
+// Library/third-party includes
+#include "gtest/gtest.h"
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/thread/scoped_thread.hpp>
+
+// Standard includes
+// - none
+
+using std::string;
+using namespace osvr::connection;
+const auto WAIT_TIME = boost::posix_time::seconds(1);
+
+inline void pleaseYield() { boost::this_thread::yield(); }
+inline void pleaseSleep() { boost::this_thread::sleep(WAIT_TIME); }
+
+TEST(AsyncAccessControl, simple) {
+    AsyncAccessControl control;
+    volatile bool sent = false;
+    ASSERT_FALSE(control.mainThreadCTS())
+        << "CTS should have no tasks waiting.";
+
+    boost::scoped_thread<> asyncThread([&] {
+        RequestToSend rts(control);
+        ASSERT_TRUE(rts.request()) << "Request should be approved";
+        sent = true;
+    });
+
+    pleaseSleep();
+    ASSERT_FALSE(sent) << "Shouldn't have been permitted to send yet.";
+    while (!control.mainThreadCTS()) {
+        pleaseYield();
+    }
+    ASSERT_TRUE(sent) << "Should have sent";
+
+    ASSERT_FALSE(control.mainThreadCTS())
+        << "CTS should have no tasks waiting.";
+}
+
+TEST(AsyncAccessControl, serialRequests) {
+    AsyncAccessControl control;
+    volatile bool sent1 = false;
+    volatile bool sent2 = false;
+    ASSERT_FALSE(control.mainThreadCTS())
+        << "CTS should have no tasks waiting.";
+
+    boost::scoped_thread<> asyncThread([&] {
+        {
+            RequestToSend rts(control);
+            ASSERT_TRUE(rts.request()) << "Request should be approved";
+            sent1 = true;
+        }
+        pleaseSleep();
+        {
+            RequestToSend rts(control);
+            ASSERT_TRUE(rts.request()) << "Request should be approved";
+            sent2 = true;
+        }
+
+    });
+
+    pleaseSleep();
+    ASSERT_FALSE(sent1) << "Shouldn't have been permitted to send first yet.";
+    ASSERT_FALSE(sent2) << "Shouldn't have been permitted to send second yet.";
+
+    while (!control.mainThreadCTS()) {
+        pleaseYield();
+    }
+    ASSERT_TRUE(sent1) << "Should have sent first.";
+    ASSERT_FALSE(sent2) << "Shouldn't have been permitted to send second yet.";
+
+    while (!control.mainThreadCTS()) {
+        pleaseYield();
+    }
+    ASSERT_TRUE(sent1) << "Should have sent first.";
+    ASSERT_TRUE(sent2) << "Should have sent second.";
+
+    ASSERT_FALSE(control.mainThreadCTS())
+        << "CTS should have no tasks waiting.";
+}
