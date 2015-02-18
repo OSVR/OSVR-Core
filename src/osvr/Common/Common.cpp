7157f291999000fc2d4f56da5ea77d6873b0d2be
Ryan Pavlik <ryan@sensics.com>
2015-02-18 11:53:14 -0600

    Add a dummy method to properly export the common library.

---
+++ src/osvr/Common/Common.cpp
@@ -0,0 +1,31 @@
+/** @file
+    @brief Implementation
+
+    @date 2015
+
+    @author
+    Sensics, Inc.
+    <http://sensics.com/osvr>
+*/
+
+// Copyright 2015 Sensics, Inc.
+//
+// All rights reserved.
+//
+// (Final version intended to be licensed under
+// the Apache License, Version 2.0)
+
+// Internal Includes
+#include <osvr/Common/Common.h>
+
+// Library/third-party includes
+// - none
+
+// Standard includes
+// - none
+
+namespace osvr {
+namespace common {
+    void dummyFunction() {}
+} // namespace common
+} // namespace osvr
