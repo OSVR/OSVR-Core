cc4fe4a04d36fde1de296ad618f3cc7503142609
Ryan Pavlik <ryan@sensics.com>
2015-02-18 12:27:44 -0600

    Extensive tests for raw serialization

--- tests/cplusplus/Common/Serialization.cpp
+++ tests/cplusplus/Common/Serialization.cpp
@@ -40,7 +40,140 @@ TEST(Serialization, BufferWrapperEmptyConstruction) {
     ASSERT_THROW(reader.readAligned(val, sizeof(val)), std::runtime_error);
 }
 
-TEST(Serialization, RawSerialization) {}
+template <typename T> class IntRawSerialization : public ::testing::Test {};
+typedef ::testing::Types<uint64_t, int64_t, uint32_t, int32_t, uint16_t,
+                         int16_t, uint8_t, int8_t> IntTypes;
+
+TYPED_TEST_CASE(IntRawSerialization, IntTypes);
+
+TYPED_TEST(IntRawSerialization, RoundTrip1) {
+    Buffer<> buf;
+    TypeParam inVal(1);
+    osvr::common::serialization::serializeRaw(buf, inVal);
+    ASSERT_EQ(buf.size(), sizeof(TypeParam));
+
+    auto reader = buf.startReading();
+
+    ASSERT_EQ(reader.bytesRemaining(), sizeof(TypeParam));
+    TypeParam outVal(0);
+    osvr::common::serialization::deserializeRaw(reader, outVal);
+    ASSERT_EQ(inVal, outVal);
+    ASSERT_EQ(reader.bytesRead(), sizeof(TypeParam));
+    ASSERT_EQ(reader.bytesRemaining(), 0);
+}
+
+TYPED_TEST(IntRawSerialization, RoundTrip0) {
+    Buffer<> buf;
+    TypeParam inVal(0);
+    osvr::common::serialization::serializeRaw(buf, inVal);
+    ASSERT_EQ(buf.size(), sizeof(TypeParam));
+
+    auto reader = buf.startReading();
+
+    ASSERT_EQ(reader.bytesRemaining(), sizeof(TypeParam));
+    TypeParam outVal(1);
+    osvr::common::serialization::deserializeRaw(reader, outVal);
+    ASSERT_EQ(inVal, outVal);
+    ASSERT_EQ(reader.bytesRead(), sizeof(TypeParam));
+    ASSERT_EQ(reader.bytesRemaining(), 0);
+}
+
+TYPED_TEST(IntRawSerialization, Endianness) {
+    Buffer<> buf;
+    TypeParam inVal(1);
+    osvr::common::serialization::serializeRaw(buf, inVal);
+    auto zeroes = buf.size() - 1;
+    for (size_t i = 0; i < zeroes; ++i) {
+        ASSERT_EQ(buf.getContents()[i], uint8_t(0));
+    }
+    ASSERT_EQ(buf.getContents()[buf.size() - 1], uint8_t(1));
+}
+
+template <typename T>
+class ArithmeticRawSerialization : public ::testing::Test {};
+typedef ::testing::Types<double, uint64_t, int64_t, uint32_t, int32_t, uint16_t,
+                         int16_t, uint8_t, int8_t> ArithmeticTypes;
+
+TYPED_TEST_CASE(ArithmeticRawSerialization, ArithmeticTypes);
+
+TYPED_TEST(ArithmeticRawSerialization, RoundTrip1) {
+    Buffer<> buf;
+    TypeParam inVal(1);
+    osvr::common::serialization::serializeRaw(buf, inVal);
+    ASSERT_EQ(buf.size(), sizeof(TypeParam));
+
+    auto reader = buf.startReading();
+
+    ASSERT_EQ(reader.bytesRemaining(), sizeof(TypeParam));
+    TypeParam outVal(0);
+    osvr::common::serialization::deserializeRaw(reader, outVal);
+    ASSERT_EQ(inVal, outVal);
+    ASSERT_EQ(reader.bytesRead(), sizeof(TypeParam));
+    ASSERT_EQ(reader.bytesRemaining(), 0);
+}
+
+TYPED_TEST(ArithmeticRawSerialization, RoundTrip0) {
+    Buffer<> buf;
+    TypeParam inVal(0);
+    osvr::common::serialization::serializeRaw(buf, inVal);
+    ASSERT_EQ(buf.size(), sizeof(TypeParam));
+
+    auto reader = buf.startReading();
+
+    ASSERT_EQ(reader.bytesRemaining(), sizeof(TypeParam));
+    TypeParam outVal(1);
+    osvr::common::serialization::deserializeRaw(reader, outVal);
+    ASSERT_EQ(inVal, outVal);
+    ASSERT_EQ(reader.bytesRead(), sizeof(TypeParam));
+    ASSERT_EQ(reader.bytesRemaining(), 0);
+}
+
+#ifdef _MSC_VER
+/// Disable "truncation of constant value" warning here.
+#pragma warning(push)
+#pragma warning(disable : 4309)
+#endif
+TYPED_TEST(ArithmeticRawSerialization, RoundTripBigger) {
+    Buffer<> buf;
+    TypeParam inVal = static_cast<TypeParam>(50.5); // OK that it will truncate
+                                                    // for integers, since we
+                                                    // compare inVal vs outVal,
+                                                    // not the literal.
+    osvr::common::serialization::serializeRaw(buf, inVal);
+    ASSERT_EQ(buf.size(), sizeof(TypeParam));
+
+    auto reader = buf.startReading();
+
+    ASSERT_EQ(reader.bytesRemaining(), sizeof(TypeParam));
+    TypeParam outVal(0);
+    osvr::common::serialization::deserializeRaw(reader, outVal);
+    ASSERT_EQ(inVal, outVal);
+    ASSERT_EQ(reader.bytesRead(), sizeof(TypeParam));
+    ASSERT_EQ(reader.bytesRemaining(), 0);
+}
+
+TYPED_TEST(ArithmeticRawSerialization, RoundTripNegative) {
+    Buffer<> buf;
+    TypeParam inVal = static_cast<TypeParam>(-50.5); // OK that it will truncate
+                                                     // for integers, since we
+                                                     // compare inVal vs outVal,
+                                                     // not the literal.
+    osvr::common::serialization::serializeRaw(buf, inVal);
+    ASSERT_EQ(buf.size(), sizeof(TypeParam));
+
+    auto reader = buf.startReading();
+
+    ASSERT_EQ(reader.bytesRemaining(), sizeof(TypeParam));
+    TypeParam outVal(0);
+    osvr::common::serialization::deserializeRaw(reader, outVal);
+    ASSERT_EQ(inVal, outVal);
+    ASSERT_EQ(reader.bytesRead(), sizeof(TypeParam));
+    ASSERT_EQ(reader.bytesRemaining(), 0);
+}
+#ifdef _MSC_VER
+/// Disable "truncation of constant value" warning here.
+#pragma warning(pop)
+#endif
 
 class MyClass {
   public:
