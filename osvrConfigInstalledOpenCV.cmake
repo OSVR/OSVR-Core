509e54bbd4395ddaeefd2df64cb2f4743b4bf26d
Ryan Pavlik <ryan@sensics.com>
2015-03-02 17:26:05 -0600

    Fix up installed imported targets that use OpenCV

---
+++ osvrConfigInstalledOpenCV.cmake
@@ -0,0 +1,19 @@
+# File included only with installed binaries, to re-find OpenCV on those systems if needed
+
+if(NOT OPENCV_FOUND)
+    if(WIN32)
+        # Hints for Windows, given the Chocolatey packages.
+        if(NOT "$ENV{ChocolateyInstall}" STREQUAL "")
+            file(TO_CMAKE_PATH "$ENV{ChocolateyInstall}/lib" CHOCO_LIB)
+            list(APPEND CMAKE_PREFIX_PATH "${CHOCO_LIB}/OpenCV/tools/opencv/build" "${CHOCO_LIB}/OpenCV.2.4.10.20150209/tools/opencv/build")
+        endif()
+    endif()
+    find_package(OpenCV QUIET COMPONENTS core)
+endif()
+
+if(OPENCV_FOUND)
+    set_property(TARGET osvr::osvrPluginKitImaging APPEND PROPERTY INTERFACE_LINK_LIBRARIES ${OpenCV_LIBS})
+    set_property(TARGET osvr::osvrPluginKitImaging APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${OpenCV_INCLUDE_DIRS})
+    set_property(TARGET osvr::osvrClientKitImaging APPEND PROPERTY INTERFACE_LINK_LIBRARIES ${OpenCV_LIBS})
+    set_property(TARGET osvr::osvrClientKitImaging APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${OpenCV_INCLUDE_DIRS})
+endif()
