4a3a725b69cd5464cc28fa03d76893a2fb63557e
Ryan Pavlik <ryan@sensics.com>
2015-03-19 11:01:38 -0500

    Greatly simplify the ValueOrRange code by simply storing everything as a range

--- inc/osvr/Util/ValueOrRange.h
+++ inc/osvr/Util/ValueOrRange.h
@@ -70,30 +70,63 @@ namespace util {
         typedef detail::ValueOrRangeIterator<ValueType> const_iterator;
         typedef const_iterator iterator;
 
+        typedef ValueType size_type;
+
         static_assert(std::is_integral<ValueType>::value,
                       "Ranges only available with integral types");
 
-        ValueOrRange()
-            : m_begin(0), m_end(m_begin + 1), m_isInitialized(false),
-              m_isRange(false) {}
+        /// @brief Default constructor: an empty range
+        ValueOrRange() : m_begin(0), m_end(m_begin) {}
 
+        /// @brief Factory method for creating a single-value range
+        static ValueOrRange SingleValue(ValueType val) {
+            return ValueOrRange(val);
+        }
+        /// @brief Factory method for creating a range from zero
+        static ValueOrRange RangeZeroTo(ValueType maxVal) {
+            return ValueOrRange(maxVal, 0);
+        }
+
+        /// @brief Factory method for creating a range from zero
+        static ValueOrRange RangeMaxMin(ValueType maxVal, ValueType minVal) {
+            return ValueOrRange(maxVal, minVal);
+        }
+
+        /// @brief Determine if the range contains the given value (treating a
+        /// value instance of this object as essentially a single-element range)
+        template <typename T> bool contains(T val) const {
+            static_assert(std::is_integral<T>::value,
+                          "Ranges only available with integral types");
+            return !empty() && getMin() <= val && val <= getMax();
+        }
+
+        /// @brief Get minimum value - closed lower bound
+        ///
+        /// Only valid if not empty()!
         ValueType getMin() const {
-            BOOST_ASSERT_MSG(isInitialized(),
-                             "Should only call if you know it's initialized!");
+            BOOST_ASSERT_MSG(!empty(), "Should only call if not empty!");
             return m_begin;
         }
 
+        /// @brief Get maximum value - closed upper bound
+        ///
+        /// Only valid if not empty()!
         ValueType getMax() const {
-            BOOST_ASSERT_MSG(isInitialized(),
-                             "Should only call if you know it's initialized!");
+            BOOST_ASSERT_MSG(!empty(), "Should only call if not empty!");
             return m_end - 1;
         }
 
-        bool isInitialized() const { return m_isInitialized; }
+        /// @brief Has the object been assigned a single value (range of size 1)
+        bool isValue() const { return (m_begin + 1) == m_end; }
 
-        bool isRange() const { return isInitialized() && m_isRange; }
+        /// @brief Has the object been assigned a range of size > 1?
+        bool isNonEmptyRange() const { return m_end > (m_begin + 1); }
 
-        bool isValue() const { return isInitialized() && !m_isRange; }
+        /// @brief Is the object an empty range? (default constructor, or
+        /// setting to a range with max < min)
+        bool empty() const { return m_begin == 0 && m_end == 0; }
+
+        size_type size() const { return m_end - m_begin; }
 
         value_type getValue() const {
             BOOST_ASSERT_MSG(isValue(), "Should only call getValue if you know "
@@ -101,68 +134,82 @@ namespace util {
             return m_begin;
         }
 
+        /// @brief Assign a single value to this object.
         void setValue(ValueType val) {
-            m_isInitialized = true;
-            m_isRange = false;
             m_begin = val;
             m_setMax(val);
         }
 
-        ValueOrRange getIntersectionWithRangeMaxMin(ValueType maxVal,
-                                                    ValueType minVal = 0) {
-            if (!isInitialized()) {
-                throw std::logic_error(
-                    "Can't intersect with a non-initialized range!");
-            }
-            ValueOrRange ret;
-            ret.setRangeMaxMin(std::min(getMax(), maxVal),
-                               std::max(getMin(), minVal));
-            return ret;
+        /// @brief Assign an empty range to this object
+        void setEmpty() {
+            m_begin = 0;
+            m_end = 0;
         }
 
+        /// @brief Assign a range of values to this object.
+        ///
+        /// Note the order of parameters to make minimum (0) optional.
+        ///
+        /// Giving a range of size == 1 is the same as calling setValue().
+        /// Giving a range where min is greater than max sets the range to be
+        /// empty, discarding your provided max and min values for sentinel
+        /// values.
         void setRangeMaxMin(ValueType maxVal, ValueType minVal = 0) {
-            if (maxVal == minVal) {
-                setValue(maxVal);
-                return;
-            }
             if (minVal > maxVal) {
-                throw std::runtime_error("Can't have a minimum value larger "
-                                         "than your maximum value!");
+                setEmpty();
             }
-
-            m_isInitialized = true;
-            m_isRange = true;
             m_begin = minVal;
             m_setMax(maxVal);
         }
 
-        void extendRangeToMax(ValueType maxVal) {
-            if (!isInitialized()) {
-                throw std::logic_error("Can't extend a non-initialized range!");
+        /// @brief Gets the (possibly empty) intersection of the ranges/values
+        ValueOrRange getIntersection(ValueOrRange other) const {
+            if (other.empty()) {
+                return other;
             }
-            if (!isRange()) {
-                throw std::logic_error("Can't extend a value as a range!");
+            if (empty()) {
+                other.setEmpty();
+                return other;
+            }
+            auto newMax = std::min(this->getMax(), other.getMax());
+            auto newMin = std::max(this->getMin(), other.getMin());
+            other.setRangeMaxMin(newMax, newMin);
+            return other;
+        }
+
+        /// @brief If this is an initialized range, extend it as needed such
+        /// that the given maxVal is included.
+        void extendRangeToMax(ValueType maxVal) {
+            if (empty()) {
+                throw std::logic_error(
+                    "Can't extend an empty range with a new max!");
             }
             if (maxVal >= m_end) {
                 m_setMax(maxVal);
             }
         }
 
-        const_iterator begin() const {
-            BOOST_ASSERT_MSG(isInitialized(), "Should only call begin() if you "
-                                              "know your value or range is "
-                                              "initialized!");
-            return const_iterator(m_begin);
-        }
+        /// @brief Get a "begin" iterator pointing to the first value in the
+        /// range (or value)
+        const_iterator begin() const { return const_iterator(m_begin); }
 
-        const_iterator end() const {
-            BOOST_ASSERT_MSG(isInitialized(), "Should only call end() if you "
-                                              "know your value or range is "
-                                              "initialized!");
-            return const_iterator(m_end);
-        }
+        /// @brief Get a "past-the-end" iterator pointing to one more than the
+        /// max value)
+        const_iterator end() const { return const_iterator(m_end); }
 
       private:
+        /// @brief Single value private constructor
+        explicit ValueOrRange(ValueType val) : m_begin(val) { m_setMax(val); }
+
+        /// @brief Range private constructor
+        ValueOrRange(ValueType maxVal, ValueType minVal) : m_begin(minVal) {
+            if (maxVal >= minVal) {
+                m_setMax(maxVal);
+            } else {
+                setEmpty();
+            }
+        }
+
         void m_setMax(ValueType maxVal) {
             /// @todo check for overflow potential before incrementing and
             /// setting
@@ -171,7 +218,6 @@ namespace util {
         ValueType m_begin;
         ValueType m_end;
         bool m_isInitialized;
-        bool m_isRange;
     };
 } // end namespace util
 
