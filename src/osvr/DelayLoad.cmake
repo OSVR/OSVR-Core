588fc0c19e5ad2edc5fb60f70d43a4572c4bbfd1
Ryan Pavlik <ryan@sensics.com>
2015-05-11 16:35:50 -0500

    Delay load the OpenCV dlls by debug name as well

--- src/osvr/DelayLoad.cmake
+++ src/osvr/DelayLoad.cmake
@@ -4,6 +4,7 @@ function(osvr_delayload_opencv _target)
         foreach(_dep ${ARGN})
             # Not perfectly elegant, but can't use generator expressions in LINK_FLAGS...
             set_property(TARGET ${_target} APPEND PROPERTY LINK_FLAGS "/DELAYLOAD:${_dep}${OpenCV_VERSION_MAJOR}${OpenCV_VERSION_MINOR}${OpenCV_VERSION_PATCH}.dll")
+            set_property(TARGET ${_target} APPEND PROPERTY LINK_FLAGS "/DELAYLOAD:${_dep}${OpenCV_VERSION_MAJOR}${OpenCV_VERSION_MINOR}${OpenCV_VERSION_PATCH}d.dll")
             target_link_libraries(${_target} PRIVATE DelayImp.lib)
         endforeach()
     endif()
