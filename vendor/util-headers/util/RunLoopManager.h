26c45f74b958f74054ccde3b46680640107cc822
Ryan Pavlik <ryan@sensics.com>
2015-05-14 14:49:27 -0500

    Update util headers

--- vendor/util-headers/util/RunLoopManager.h
+++ vendor/util-headers/util/RunLoopManager.h
@@ -2,14 +2,14 @@
 		@brief Header
 
 		This header is maintained as a part of 'util-headers' - you can always
-	find the latest version online at https://github.com/vancegroup/util-headers
+    find the latest version online at https://github.com/rpavlik/util-headers
 
-	This GUID can help identify the project: d1dbc94e-e863-49cf-bc08-ab4d9f486613
+    This GUID can help identify the project: d1dbc94e-e863-49cf-bc08-ab4d9f486613
 
-	This copy of the header is from the revision that Git calls
-	a1072ffcb5b18913c2a154020e16305ffc11842c
+   This copy of the header is from the revision that Git calls
+    5574e8f134c953c687a741043067a9082120b497
 
-	Commit date: "2014-11-11 15:20:21 -0600"
+    Commit date: "2015-05-14 14:47:19 -0500"
 
 		@date 2013
 
@@ -40,117 +40,121 @@
 // - none
 
 namespace util {
-class StartingInterface {
-public:
-	virtual void signalStart() = 0;
-	virtual void signalAndWaitForStart() = 0;
-};
+	class StartingInterface {
+		public:
+			virtual void signalStart() = 0;
+			virtual void signalAndWaitForStart() = 0;
+	};
 
-class LoopInterface {
-public:
-	virtual void reportStarting() = 0;
-	virtual void reportRunning() = 0;
-	virtual bool shouldContinue() = 0;
-	virtual void reportStopped() = 0;
-};
+	class LoopInterface {
+		public:
+			virtual void reportStarting() = 0;
+			virtual void reportRunning() = 0;
+			virtual bool shouldContinue() = 0;
+			virtual void reportStopped() = 0;
+	};
 
-class ShutdownInterface {
-public:
-	virtual void signalShutdown() = 0;
-	virtual void signalAndWaitForShutdown() = 0;
-};
+	class ShutdownInterface {
+		public:
+			virtual void signalShutdown() = 0;
+			virtual void signalAndWaitForShutdown() = 0;
+	};
 
 /// @brief Base class for implementations of a RunLoopManager that use
 /// various synchronization libraries.
-class RunLoopManagerBase : public StartingInterface,
-						   public LoopInterface,
-						   public ShutdownInterface,
-						   boost::noncopyable {
-public:
-	enum RunningState { STATE_STOPPED, STATE_STARTING, STATE_RUNNING };
-	RunLoopManagerBase() : shouldStop_(false) {}
+	class RunLoopManagerBase : public StartingInterface,
+		public LoopInterface,
+		public ShutdownInterface,
+		boost::noncopyable {
+		public:
+			enum RunningState { STATE_STOPPED, STATE_STARTING, STATE_RUNNING };
+			RunLoopManagerBase() : shouldStop_(false) {}
 
-	/// @name Starting Interface
-	/// @{
-	/// @brief Set up internal state to allow the run loop to start.
-	virtual void signalStart() = 0;
-	/// @brief Set up for run loop to start, then block until we have
-	/// confirmation that the run loop is running.
-	virtual void signalAndWaitForStart() = 0;
-	/// @}
+			/// @name Starting Interface
+			/// @{
+			/// @brief Set up internal state to allow the run loop to start.
+			virtual void signalStart() = 0;
+			/// @brief Set up for run loop to start, then block until we have
+			/// confirmation that the run loop is running.
+			virtual void signalAndWaitForStart() = 0;
+			/// @}
 
-	/// @name ShutdownInterface
-	/// @{
-	/// @brief Send a message to the run loop that it should stop.
-	virtual void signalShutdown() = 0;
-	/// @brief Send a message to the run loop that it should stop, and wait
-	/// until it does.
-	virtual void signalAndWaitForShutdown() = 0;
-	/// @}
+			/// @name ShutdownInterface
+			/// @{
+			/// @brief Send a message to the run loop that it should stop.
+			virtual void signalShutdown() = 0;
+			/// @brief Send a message to the run loop that it should stop, and wait
+			/// until it does.
+			virtual void signalAndWaitForShutdown() = 0;
+			/// @}
 
-	/// @name Loop Interface
-	/// @brief Methods called from within the run loop or by a helper class.
-	/// @{
-	/// @brief Notify the run loop manager that the run loop is now starting but
-	/// not yet running.
-	void reportStarting();
-	/// @brief Notify the run loop manager that the run loop is now running.
-	void reportRunning();
-	bool shouldContinue();
-	void reportStopped();
-	/// @}
+			/// @name Loop Interface
+			/// @brief Methods called from within the run loop or by a helper class.
+			/// @{
+			/// @brief Notify the run loop manager that the run loop is now starting but
+			/// not yet running.
+			void reportStarting();
+			/// @brief Notify the run loop manager that the run loop is now running.
+			void reportRunning();
+			bool shouldContinue();
+			void reportStopped();
+			/// @}
 
-protected:
-	/// @brief Changes the internal state variable (under mutex protection) and
-	/// notifies anyone who might be waiting on a condition change.
-	virtual void reportStateChange_(RunningState s) = 0;
+		protected:
+			/// @brief Changes the internal state variable (under mutex protection) and
+			/// notifies anyone who might be waiting on a condition change.
+			virtual void reportStateChange_(RunningState s) = 0;
 
-	/// @brief internal utility function.
-	void setShouldStop_(bool value); ///< shared implementation provided
+			/// @brief internal utility function.
+			void setShouldStop_(bool value); ///< shared implementation provided
 
-private:
-	/// One-way signalling flag from outside to the runloop.
-	volatile bool shouldStop_;
-};
+		private:
+			/// One-way signalling flag from outside to the runloop.
+			volatile bool shouldStop_;
+	};
 
 /// @brief RAII class to signal loop start and end.
-class LoopGuard : boost::noncopyable {
-public:
-	enum StartTime { REPORT_START_IMMEDIATELY, DELAY_REPORTING_START };
-	LoopGuard(LoopInterface &mgr, StartTime t = REPORT_START_IMMEDIATELY);
-	~LoopGuard();
+	class LoopGuard : boost::noncopyable {
+		public:
+			enum StartTime { REPORT_START_IMMEDIATELY, DELAY_REPORTING_START };
+			LoopGuard(LoopInterface &mgr, StartTime t = REPORT_START_IMMEDIATELY);
+			~LoopGuard();
 
-private:
-	LoopInterface &mgr_;
-};
+		private:
+			LoopInterface &mgr_;
+	};
 
-inline void RunLoopManagerBase::reportStarting() {
-	reportStateChange_(STATE_STARTING);
-}
-
-inline void RunLoopManagerBase::reportRunning() {
-	reportStateChange_(STATE_RUNNING);
-}
-
-inline bool RunLoopManagerBase::shouldContinue() { return !shouldStop_; }
-
-inline void RunLoopManagerBase::reportStopped() {
-	reportStateChange_(STATE_STOPPED);
-}
-
-inline void RunLoopManagerBase::setShouldStop_(bool value) {
-	shouldStop_ = value;
-}
-
-inline LoopGuard::LoopGuard(LoopInterface &mgr, LoopGuard::StartTime t)
-	: mgr_(mgr) {
-	mgr_.reportStarting();
-	if (t == REPORT_START_IMMEDIATELY) {
-		mgr_.reportRunning();
+	inline void RunLoopManagerBase::reportStarting() {
+		reportStateChange_(STATE_STARTING);
 	}
-}
 
-inline LoopGuard::~LoopGuard() { mgr_.reportStopped(); }
+	inline void RunLoopManagerBase::reportRunning() {
+		reportStateChange_(STATE_RUNNING);
+	}
+
+	inline bool RunLoopManagerBase::shouldContinue() {
+		return !shouldStop_;
+	}
+
+	inline void RunLoopManagerBase::reportStopped() {
+		reportStateChange_(STATE_STOPPED);
+	}
+
+	inline void RunLoopManagerBase::setShouldStop_(bool value) {
+		shouldStop_ = value;
+	}
+
+	inline LoopGuard::LoopGuard(LoopInterface &mgr, LoopGuard::StartTime t)
+		: mgr_(mgr) {
+		mgr_.reportStarting();
+		if (t == REPORT_START_IMMEDIATELY) {
+			mgr_.reportRunning();
+		}
+	}
+
+	inline LoopGuard::~LoopGuard() {
+		mgr_.reportStopped();
+	}
 
 } // end of namespace util
 
