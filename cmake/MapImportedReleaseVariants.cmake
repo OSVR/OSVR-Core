95f5117db7ac0a22af9b8a5159a4a63fcf3af104
Ryan Pavlik <ryan@sensics.com>
2015-05-14 11:15:23 -0500

    Correctly use "Sensics, Inc." where appropriate.

--- cmake/MapImportedReleaseVariants.cmake
+++ cmake/MapImportedReleaseVariants.cmake
@@ -6,7 +6,7 @@
 # 2015 Ryan Pavlik <ryan@sensics.com> <abiryan@ryand.net>
 # http://academic.cleardefinition.com
 #
-# Copyright Sensics 2015.
+# Copyright Sensics, Inc. 2015.
 # Distributed under the Boost Software License, Version 1.0.
 # (See accompanying file LICENSE_1_0.txt or copy at
 # http://www.boost.org/LICENSE_1_0.txt)
