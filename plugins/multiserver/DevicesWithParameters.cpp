af3681800c22bce829dc121deeaf749dde49c690
Ryan Pavlik <ryan@sensics.com>
2015-05-20 12:30:07 -0500

    Fixed multiserver build without usbserialenum, and normalized behavior.
    Now, if you pass a port (or if you don't have USB serial enum), it will only instantiate that one.
    
    Otherwise, if you have enum, it will instantiate all of the devices.

--- plugins/multiserver/DevicesWithParameters.cpp
+++ plugins/multiserver/DevicesWithParameters.cpp
@@ -26,7 +26,9 @@
 #include "DevicesWithParameters.h"
 #include "GetSerialPortState.h"
 #include <osvr/VRPNServer/VRPNDeviceRegistration.h>
+#ifdef OSVR_HAVE_USBSERIALENUM
 #include <osvr/USBSerial/USBSerialEnum.h>
+#endif
 #include <osvr/Util/StringLiteralFileToString.h>
 #include "com_osvr_Multiserver_YEI_3Space_Sensor_json.h"
 
@@ -78,6 +80,42 @@ class CStringArray : boost::noncopyable {
 };
 } // namespace
 
+static void createYEIImpl(VRPNMultiserverData &data, OSVR_PluginRegContext ctx,
+                          Json::Value const &root, std::string port) {
+    if (port.empty()) {
+        throw std::runtime_error(
+            "Could not create a YEI device: no port specified!.");
+    }
+    port = normalizeAndVerifySerialPort(port);
+
+    bool calibrate_gyros_on_setup =
+        root.get("calibrateGyrosOnSetup", false).asBool();
+    bool tare_on_setup = root.get("tareOnSetup", false).asBool();
+    double frames_per_second = root.get("framesPerSecond", 250).asFloat();
+
+    Json::Value commands = root.get("resetCommands", Json::arrayValue);
+    CStringArray reset_commands;
+
+    if (commands.empty()) {
+        // Enable Q-COMP filtering by default
+        reset_commands.push_back("123,2");
+    } else {
+        for (Json::ArrayIndex i = 0, e = commands.size(); i < e; ++i) {
+            reset_commands.push_back(commands[i].asString());
+        }
+    }
+
+    osvr::vrpnserver::VRPNDeviceRegistration reg(ctx);
+
+    reg.registerDevice(new vrpn_YEI_3Space_Sensor(
+        reg.useDecoratedName(data.getName("YEI_3Space_Sensor")).c_str(),
+        reg.getVRPNConnection(), port.c_str(), 115200, calibrate_gyros_on_setup,
+        tare_on_setup, frames_per_second, 0, 0, 1, 0,
+        reset_commands.get_array()));
+    reg.setDeviceDescriptor(
+        osvr::util::makeString(com_osvr_Multiserver_YEI_3Space_Sensor_json));
+}
+
 void createYEI(VRPNMultiserverData &data, OSVR_PluginRegContext ctx,
                const char *params) {
     Json::Reader reader;
@@ -86,37 +124,21 @@ void createYEI(VRPNMultiserverData &data, OSVR_PluginRegContext ctx,
         throw std::runtime_error("Could not parse configuration: " +
                                  reader.getFormattedErrorMessages());
     }
-    static const uint16_t vID = 0x9AC;
-    static const uint16_t pID = 0x3F2;
-    for (auto dev : osvr::usbserial::Enumerator(vID, pID)) {
-
-        std::string port = normalizeAndVerifySerialPort(dev->getPort());
-
-        bool calibrate_gyros_on_setup =
-            root.get("calibrateGyrosOnSetup", false).asBool();
-        bool tare_on_setup = root.get("tareOnSetup", false).asBool();
-        double frames_per_second = root.get("framesPerSecond", 250).asFloat();
-
-        Json::Value commands = root.get("resetCommands", Json::arrayValue);
-        CStringArray reset_commands;
-
-        if (commands.empty()) {
-            // Enable Q-COMP filtering by default
-            reset_commands.push_back("123,2");
-        } else {
-            for (Json::ArrayIndex i = 0, e = commands.size(); i < e; ++i) {
-                reset_commands.push_back(commands[i].asString());
-            }
+    auto port = root.get("port", "").asString();
+#ifdef OSVR_HAVE_USBSERIALENUM
+    // Have usb serial enum: specifying a port means you just want that one,
+    // not specifying means you want it to auto-configure all.
+    if (!port.empty()) {
+        createYEIImpl(data, ctx, root, port);
+    } else {
+        static const uint16_t vID = 0x9AC;
+        static const uint16_t pID = 0x3F2;
+        for (auto dev : osvr::usbserial::Enumerator(vID, pID)) {
+            createYEIImpl(data, ctx, root, dev->getPort());
         }
-
-        osvr::vrpnserver::VRPNDeviceRegistration reg(ctx);
-
-        reg.registerDevice(new vrpn_YEI_3Space_Sensor(
-            reg.useDecoratedName(data.getName("YEI_3Space_Sensor")).c_str(),
-            reg.getVRPNConnection(), port.c_str(), 115200,
-            calibrate_gyros_on_setup, tare_on_setup, frames_per_second, 0, 0, 1,
-            0, reset_commands.get_array()));
-        reg.setDeviceDescriptor(osvr::util::makeString(
-            com_osvr_Multiserver_YEI_3Space_Sensor_json));
     }
+#else  // !OSVR_HAVE_USBSERIALENUM
+    // No usb serial enum: must unconditionally specify a port.
+    createYEIImpl(data, ctx, root, port);
+#endif // OSVR_HAVE_USBSERIALENUM
 }
