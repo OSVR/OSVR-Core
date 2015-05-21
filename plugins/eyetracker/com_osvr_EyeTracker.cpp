8f09e78e966860ccea513fab19e2e8c985ce198f
Georgiy Frolov <mars979@gmail.com>
2015-05-21 15:03:14 -0400

    add data reports for second sensor to eye tracker plugin

--- plugins/eyetracker/com_osvr_EyeTracker.cpp
+++ plugins/eyetracker/com_osvr_EyeTracker.cpp
@@ -32,6 +32,8 @@
 // Standard includes
 #include <iostream>
 #include <memory>
+#include <ctime>
+
 
 // Anonymous namespace to avoid symbol collision
 namespace {
@@ -47,7 +49,7 @@ class EyeTrackerDevice {
         OSVR_DeviceInitOptions opts = osvrDeviceCreateInitOptions(ctx);
 
         //tracker = trackerInst;
-		osvrDeviceEyeTrackerConfigure(opts, &m_eyetracker, 1);
+		osvrDeviceEyeTrackerConfigure(opts, &m_eyetracker, 2);
 
         /// Create the sync device token with the options
         m_dev.initSync(ctx, "EyeTracker", opts);
@@ -61,29 +63,57 @@ class EyeTrackerDevice {
 
     OSVR_ReturnCode update() {
 
-		OSVR_EyeGazeDirection gaze;
+		OSVR_EyeGazeDirection leftGaze;
+		OSVR_EyeGazeDirection rightGaze;
 		OSVR_TimeValue times;
 
 		osvrTimeValueGetNow(&times);
 
+		std::srand(std::time(0));
+		int randVal = std::rand();
+
+
+		//Left eye Data
 		OSVR_Vec2 gaze2D;
-		gaze2D.data[0] = 0.5;
-		gaze2D.data[1] = 0.5;
+		gaze2D.data[0] = std::abs(std::sin(randVal));
+		gaze2D.data[1] = std::abs(std::cos(randVal));
 
 		OSVR_Vec3 gaze3D;
-		gaze3D.data[0] = 1.5;
-		gaze3D.data[1] = 1.5;
-		gaze3D.data[2] = 1.5;
+		gaze3D.data[0] = std::abs( std::sin(randVal) / (std::cos(randVal)));
+		gaze3D.data[1] = std::abs( std::cos(randVal) / (std::sin(randVal)));
+		gaze3D.data[2] = std::abs( std::sin(randVal) *  std::sin(randVal));
 
-		gaze.gazeDirection2D = gaze2D;
-		gaze.gazeDirection3D = gaze3D;
+		leftGaze.gazeDirection2D = gaze2D;
+		leftGaze.gazeDirection3D = gaze3D;
+		//printReport(&leftGaze, 1);
+		osvrDeviceEyeTrackerReportData(m_dev, m_eyetracker, leftGaze, 1, &times);
 
+		gaze2D.data[0] = -99;
+		gaze2D.data[1] = -99;
 
-		//m_dev.send(m_eyetracker, osvr::pluginkit::EyeTrackerMessage(gaze));
-		osvrDeviceEyeTrackerReportData(m_dev, m_eyetracker, gaze, 1, &times);
-        
+		gaze3D.data[0] = -99;
+		gaze3D.data[1] = -99;
+		gaze3D.data[2] = -99;
+
+		rightGaze.gazeDirection2D = gaze2D;
+		rightGaze.gazeDirection3D = gaze3D;
+		//printReport(&rightGaze, 2);
+		osvrDeviceEyeTrackerReportData(m_dev, m_eyetracker, rightGaze, 2, &times);
+		        
         return OSVR_RETURN_SUCCESS;
     }
+
+	void printReport(OSVR_EyeGazeDirection *report, int sensor){
+		std::cout << "Sensor " << sensor << ", "
+			"2D Report: " <<
+			"x: " << report->gazeDirection2D.data[0] << ", " <<
+			"y: " << report->gazeDirection2D.data[1] << std::endl;
+
+		std::cout << "3D Report: " <<
+			"x: " << report->gazeDirection3D.data[0] << ", " <<
+			"y: " << report->gazeDirection3D.data[1] << ", " <<
+			"z: " << report->gazeDirection3D.data[2] << std::endl;
+	}
 
   private:
     osvr::pluginkit::DeviceToken m_dev;
@@ -95,12 +125,13 @@ class HardwareDetection {
 	  HardwareDetection() : m_found(false) {}
     OSVR_ReturnCode operator()(OSVR_PluginRegContext ctx) {
 
-        std::cout << "PLUGIN: Got a hardware detection request" << std::endl;
-
 		if (m_found) {
 			return OSVR_RETURN_SUCCESS;
 		}
+
+		std::cout << "PLUGIN: Got a hardware detection request" << std::endl;
         
+		//we always detect an eye tracker in sample plugin
 		if (0 == 1){
 			std::cout << "PLUGIN: We have NOT detected Eye Tracker "
                       << std::endl;
@@ -116,9 +147,6 @@ class HardwareDetection {
 
         return OSVR_RETURN_SUCCESS;
     }
-
-    //TrackerPtr tracker;
-
 
 private:
 	bool m_found;
