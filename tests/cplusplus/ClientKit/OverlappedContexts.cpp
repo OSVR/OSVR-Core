6ce841806577f0ffec848ecb92292f690cb05bf1
Ryan Pavlik <ryan@sensics.com>
2015-06-19 10:02:27 -0500

    Add a test for overlapped-lifetime contexts.
    Simulating a unity scenario.
    Also adjusted the build for these clientkit tests to reduce duplication.

---
+++ tests/cplusplus/ClientKit/OverlappedContexts.cpp
@@ -0,0 +1,43 @@
+/** @file
+    @brief Test Implementation
+
+    @date 2015
+
+    @author
+    Sensics, Inc.
+    <http://sensics.com/osvr>
+
+*/
+
+// Copyright 2015 Sensics, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Internal Includes
+#include <osvr/ClientKit/Context.h>
+
+// Library/third-party includes
+// - none
+
+// Standard includes
+#include "gtest/gtest.h"
+
+TEST(OverlappedContexts, TwoContexts) {
+    OSVR_ClientContext firstContext =
+        osvrClientInit("com.osvr.test.firstContext");
+    OSVR_ClientContext secondContext =
+        osvrClientInit("com.osvr.test.secondContext");
+    ASSERT_EQ(OSVR_RETURN_SUCCESS, osvrClientShutdown(firstContext));
+    osvrClientUpdate(secondContext);
+    ASSERT_EQ(OSVR_RETURN_SUCCESS, osvrClientShutdown(secondContext));
+}
