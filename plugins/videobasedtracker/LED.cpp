1f0b8fb1dd2467f6a8bb7f0b043bb0be42f21ab5
Ryan Pavlik <ryan@sensics.com>
2015-06-25 10:49:11 -0500

    Clean up a loop.

--- plugins/videobasedtracker/LED.cpp
+++ plugins/videobasedtracker/LED.cpp
@@ -64,16 +64,22 @@ namespace vbtracker {
             return end(keypoints);
         }
 
+        auto location = m_location;
+        /// @todo can we use squared-norm instead of doing a square-root inside
+        /// of a tight loop like this?
+        auto computeDist = [location](KeyPointIterator it) {
+            return sqrt(norm(location - it->pt));
+        };
+
         // Find the distance to the first point and record it as the
         // current minimum distance;
         auto ret = begin(keypoints);
-        double minDist = sqrt(norm(m_location - ret->pt));
+        auto minDist = computeDist(ret);
 
         // Search the rest of the elements to see if we can find a
         // better one.
-        auto it = keypoints.begin();
-        while (++it != keypoints.end()) {
-            double dist = sqrt(norm(m_location - it->pt));
+        for (auto it = begin(keypoints), e = end(keypoints); it != e; ++it) {
+            auto dist = computeDist(it);
             if (dist < minDist) {
                 minDist = dist;
                 ret = it;
@@ -84,9 +90,8 @@ namespace vbtracker {
         // return the end.
         if (minDist <= threshold) {
             return ret;
-        } else {
-            return end(keypoints);
         }
+        return end(keypoints);
     }
 
 } // End namespace vbtracker
