3d5cdd4267b1b5ddb3aad72e5b6befcc49126d6c
Russ Taylor <russ@reliasolve.com>
2015-07-16 08:38:47 -0400

    Pulling in external open-source code from UNC CISMM Video project and modifying to suit our needs here.

---
+++ plugins/videobasedtracker/DirectShowCameraLib/directx_camera_server.h
@@ -0,0 +1,128 @@
+/** @file
+@brief Header
+
+@date 2015
+
+@author
+Russ Taylor working through ReliaSolve for Sensics.
+Sensics, Inc.
+<http://sensics.com/osvr>
+*/
+
+// Copyright 2015 Sensics, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifdef _WIN32
+#ifndef INCLUDED_directx_camera_server_h_GUID_9322F126_0DA4_4DB9_11F3_DDBF76A6D9D9
+#define INCLUDED_directx_camera_server_h_GUID_9322F126_0DA4_4DB9_11F3_DDBF76A6D9D9
+
+
+// Internal Includes
+#include "base_camera_server.h"
+
+// Library/third-party includes
+// - none
+
+// Standard includes
+#include <windows.h>
+
+// Horrible hack to get around missing file in Platform SDK
+#pragma include_alias( "dxtrans.h", "qedit.h" )
+#define __IDxtCompositor_INTERFACE_DEFINED__
+#define __IDxtAlphaSetter_INTERFACE_DEFINED__
+#define __IDxtJpeg_INTERFACE_DEFINED__
+#define __IDxtKey_INTERFACE_DEFINED__
+
+// Include files for DirectShow video input
+#include <dshow.h>
+#include <qedit.h>
+
+// This code (and the code in the derived videofile server) is
+// based on information the book "Programming Microsoft DirectShow
+// for Digital Video and Television", Mark D. Pesce.  Microsoft Press.
+// ISBN 0-7356-1821-6.  This book was required reading for me to
+// understand how the filter graphs run, how to control replay rate,
+// and how to reliably grab all of the samples from a stream.
+// The chapter on using the Sample Grabber Filter
+// (ch. 11) was particularly relevant.  Note that I did not break the seal
+// on the enclosed disk, which would require me to agree to licensing terms
+// that include me not providing anyone with copies of my modified versions
+// of the sample code in source-code format.  Instead, I used the information
+// gained in the reading of the book to write by hand my own version of this
+// code.  What a sick world we live in when example code can't be freely
+// shared.
+
+// The Microsoft Platform SDK must be installed on your machine
+// to make this work; this code makes use of the BaseClasses library and
+// other parts of the library.
+
+class directx_samplegrabber_callback; //< Forward declaration
+
+class directx_camera_server : public base_camera_server {
+public:
+  /// Open the nth available camera.  First camera is 1.
+  directx_camera_server(int which, unsigned width = 0, unsigned height = 0);
+  virtual ~directx_camera_server(void);
+
+  /// Return the number of colors that the device has
+  virtual unsigned  get_num_colors() const { return 3; };
+
+  /// Read an image to a memory buffer.  Max < min means "whole range"
+  virtual bool	read_image_to_memory(unsigned minX = 255, unsigned maxX = 0,
+			     unsigned minY = 255, unsigned maxY = 0,
+			     double exposure_millisecs = 250.0);
+
+  /// Get pixels out of the memory buffer, RGB indexes the colors
+  virtual bool	get_pixel_from_memory(unsigned X, unsigned Y, vrpn_uint8 &val, int RGB = 0) const;
+  virtual bool	get_pixel_from_memory(unsigned X, unsigned Y, vrpn_uint16 &val, int RGB = 0) const;
+
+  /// Get a pointer to the actual memory buffer.
+  /// @todo Construct a safer way to access this.
+  const unsigned char *get_pixel_buffer_pointer(void) const { return _buffer; }
+
+protected:
+  virtual void close_device(void);
+
+  /// Construct but do not open camera (used by derived classes)
+  directx_camera_server();
+
+  // Objects needed for DirectShow video input.
+  IGraphBuilder *_pGraph;	      // Constructs a DirectShow filter graph
+  ICaptureGraphBuilder2 *_pBuilder;   // Filter graph builder
+  IMediaControl *_pMediaControl;      // Handles media streaming in the filter graph
+  IMediaEvent   *_pEvent;	      // Handles filter graph events
+  IBaseFilter *_pSampleGrabberFilter; // Grabs samples from the media stream
+  ISampleGrabber *_pGrabber;	      // Interface for the sample grabber filter
+  IAMStreamConfig *_pStreamConfig;    // Interface to set the video dimensions
+
+  // Memory pointers used to get non-virtual memory
+  unsigned char	*_buffer;   //< Buffer for what comes from camera,
+  size_t    _buflen;	    //< Length of that buffer
+  bool	    _started_graph; //< Did we start the filter graph running?
+  unsigned  _mode;	    //< Mode 0 = running, Mode 1 = paused.
+
+  long	    _stride;	    //< How many bytes to skip when going to next line (may be negative for upside-down images)
+
+  // Pointer to the associated sample grabber callback object.
+  directx_samplegrabber_callback  *_pCallback;
+
+  virtual bool	open_and_find_parameters(const int which, unsigned width, unsigned height);
+  virtual bool	read_one_frame(unsigned minX, unsigned maxX,
+			unsigned minY, unsigned maxY,
+			unsigned exposure_millisecs);
+};
+
+#endif // INCLUDED_directx_camera_server_h_GUID_9322F126_0DA4_4DB9_11F3_DDBF76A6D9D9
+#endif // _WIN32
+
