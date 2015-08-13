7865bf111d0e905d2dd57ed1ba71002fa7ff0e73
Ryan Pavlik <ryan@sensics.com>
2015-08-13 14:30:10 -0500

    Change rect to just use a std::array instead of eigen, to simplify (no need for alignment constraints)

--- inc/osvr/Util/Rect.h
+++ inc/osvr/Util/Rect.h
@@ -26,31 +26,28 @@
 #define INCLUDED_Rect_h_GUID_A81C8929_371D_4EBC_C6BB_A7264EF642E6
 
 // Internal Includes
-#include <osvr/Util/EigenCoreGeometry.h>
+// - none
 
 // Library/third-party includes
 // - none
 
 // Standard includes
-// - none
+#include <array>
 
 namespace osvr {
 namespace util {
     template <typename Scalar = double> class Rect {
       public:
-        EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
-
         typedef Rect type;
         typedef Scalar value_type;
-        typedef Eigen::Matrix<Scalar, 4, 1> storage_type;
         enum Side { LEFT = 0, RIGHT = 1, TOP = 2, BOTTOM = 3 };
 
         /// @brief Access by side
         value_type &operator[](Side s) {
-            return m_data[static_cast<storage_type::Index>(s)];
+            return m_data[static_cast<index_type>(s)];
         }
         value_type operator[](Side s) const {
-            return m_data[static_cast<storage_type::Index>(s)];
+            return m_data[static_cast<index_type>(s)];
         }
 
         /// @brief Chained function call operator for setting sides.
@@ -59,10 +56,17 @@ namespace util {
             return *this;
         }
 
-        storage_type &data() { return m_data; }
-        storage_type const &data() const { return m_data; }
+        /// @brief Componentwise multiplication by scalar
+        type &operator*=(Scalar v) {
+            for (auto &e : m_data) {
+                e *= v;
+            }
+            return *this;
+        }
 
       private:
+        typedef std::array<Scalar, 4> storage_type;
+        typedef std::size_t index_type;
         storage_type m_data;
     };
     typedef Rect<> Rectd;
