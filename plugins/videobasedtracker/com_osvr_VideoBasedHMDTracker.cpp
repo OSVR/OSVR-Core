bed830adc0893157324d2c71e0504d1fb355a8b3
Ryan Pavlik <ryan@sensics.com>
2015-09-10 16:27:59 -0500

    clang-format.

--- plugins/videobasedtracker/com_osvr_VideoBasedHMDTracker.cpp
+++ plugins/videobasedtracker/com_osvr_VideoBasedHMDTracker.cpp
@@ -48,7 +48,8 @@
 
 // Define the constant below to use a DirectShow-based workaround to not
 // being able to open the OSVR HDK camera using OpenCV.
-/// @todo Remove this code and the DirectShow stuff once the camera can be read by OpenCV
+/// @todo Remove this code and the DirectShow stuff once the camera can be read
+/// by OpenCV
 #define VBHMD_USE_DIRECTSHOW
 #ifdef VBHMD_USE_DIRECTSHOW
 #include "directx_camera_server.h"
@@ -90,9 +91,8 @@ class VideoBasedHMDTracker : boost::noncopyable {
         cv::setNumThreads(1);
 
         // Initialize things from parameters and from defaults.  Do it here
-        // rather than
-        // in an initialization list so that we're independent of member order
-        // declaration.
+        // rather than in an initialization list so that we're independent of
+        // member order declaration.
         m_channel = channel;
         m_type = Unknown;
 
@@ -185,16 +185,14 @@ class VideoBasedHMDTracker : boost::noncopyable {
         //        m_identifiers.push_back(new
         //        osvr::vbtracker::OsvrHdkLedIdentifier(osvr::vbtracker::OsvrHdkLedIdentifier_RANDOM_IMAGES_PATTERNS));
 
-        m_vbtracker.addSensor(
-            osvr::vbtracker::createHDKLedIdentifier(0),
-            m, d, osvr::vbtracker::OsvrHdkLedLocations_SENSOR0,
-            4, 2);
+        m_vbtracker.addSensor(osvr::vbtracker::createHDKLedIdentifier(0), m, d,
+                              osvr::vbtracker::OsvrHdkLedLocations_SENSOR0, 4,
+                              2);
         // There are sometimes only four beacons on the back unit (two of
         // the LEDs are disabled), so we let things work with just those.
-        m_vbtracker.addSensor(
-            osvr::vbtracker::createHDKLedIdentifier(1),
-            m, d, osvr::vbtracker::OsvrHdkLedLocations_SENSOR1,
-            4, 0);
+        m_vbtracker.addSensor(osvr::vbtracker::createHDKLedIdentifier(1), m, d,
+                              osvr::vbtracker::OsvrHdkLedLocations_SENSOR1, 4,
+                              0);
 
 #else
 #ifdef VBHMD_USE_DIRECTSHOW
@@ -209,8 +207,8 @@ class VideoBasedHMDTracker : boost::noncopyable {
 #ifdef VBHMD_USE_DIRECTSHOW
             int minx, miny, maxx, maxy;
             m_camera.read_range(minx, maxx, miny,maxy);
-            height = maxy - miny+1;
-            width = maxx - minx+1;
+            height = maxy - miny + 1;
+            width = maxx - minx + 1;
 #else
             height = static_cast<int>(m_camera.get(CV_CAP_PROP_FRAME_HEIGHT));
             width = static_cast<int>(m_camera.get(CV_CAP_PROP_FRAME_WIDTH));
@@ -228,8 +226,8 @@ class VideoBasedHMDTracker : boost::noncopyable {
                 m_type = OSVRHDK;
             }
 #ifdef VBHMD_DEBUG
-            std::cout << "Got image from camera of size " << width << "x" << height
-                      << std::endl;
+            std::cout << "Got image from camera of size " << width << "x"
+                      << height << std::endl;
 #endif
             if (m_type == OculusDK2) {
                 std::cout << "Is Oculus camera, reformatting to mono"
@@ -274,25 +272,27 @@ class VideoBasedHMDTracker : boost::noncopyable {
             //  tan (hFOV / 2) = 0.5 * W / FL
             //  2 * tan (hFOV/2) = W / FL
             //  FL = W / ( 2 * tan(hFOV/2) )
-            // We want the focal length in units of pixels.  The manufacturer tells
-            // us that the optical FOV is 82.9 degrees; we assume that this is a
-            // diagonal measurement.  Since the camera resolution is 640x480 pixels,
-            // the diagonal is sqrt( 640*640 + 480*480 ) = 800.
-            // If we use the equation above, but compute the diagonal focal length
-            // (they are linearly related), we get
+            // We want the focal length in units of pixels.  The manufacturer
+            // tells us that the optical FOV is 82.9 degrees; we assume that
+            // this is a diagonal measurement.  Since the camera resolution is
+            // 640x480 pixels, the diagonal is sqrt( 640*640 + 480*480 ) = 800.
+            // If we use the equation above, but compute the diagonal focal
+            // length (they are linearly related), we get
             //  FL = 800 / ( 2 * tan(82.9/2) )
             //  FL = 452.9
-            // Testing with the tracked position when the unit was about 10 inches
-            // from the camera and nearly centered produced a Z estimate of 0.255,
-            // where 0.254 is expected.  This is well within the ruler-based method
-            // of estimating 10 inches, so seems to be correct.
-            // The manufacturer specs distortion < 3% on the module and 1.5% on the
-            // lens, so we ignore the distortion and put in 0 coefficients.
+            // Testing with the tracked position when the unit was about 10
+            // inches from the camera and nearly centered produced a Z estimate
+            // of 0.255, where 0.254 is expected.  This is well within the
+            // ruler-based method of estimating 10 inches, so seems to be
+            // correct.
+            // The manufacturer specs distortion < 3% on the module and 1.5% on
+            // the lens, so we ignore the distortion and put in 0 coefficients.
             /// @todo Come up with actual estimates for distortion
             /// parameters by calibrating them in OpenCV.
             double cx = width / 2.0;
             double cy = height / 2.0;
-            double fx = 452.9; // 700.0; // XXX This needs to be in pixels, not mm
+            double fx =
+                452.9; // 700.0; // XXX This needs to be in pixels, not mm
             double fy = fx;
             std::vector<std::vector<double> > m;
             m.push_back({fx, 0.0, cx});
@@ -304,14 +304,12 @@ class VideoBasedHMDTracker : boost::noncopyable {
             d.push_back(0);
             d.push_back(0);
             d.push_back(0);
-            m_vbtracker.addSensor(osvr::vbtracker::createHDKLedIdentifier(0), m,
-                                  d,
-                                  osvr::vbtracker::OsvrHdkLedLocations_SENSOR0,
-                                  6, 0);
-            m_vbtracker.addSensor(osvr::vbtracker::createHDKLedIdentifier(1), m,
-                                  d,
-                                  osvr::vbtracker::OsvrHdkLedLocations_SENSOR1,
-                                  4, 0);
+            m_vbtracker.addSensor(
+                osvr::vbtracker::createHDKLedIdentifier(0), m, d,
+                osvr::vbtracker::OsvrHdkLedLocations_SENSOR0, 6, 0);
+            m_vbtracker.addSensor(
+                osvr::vbtracker::createHDKLedIdentifier(1), m, d,
+                osvr::vbtracker::OsvrHdkLedLocations_SENSOR1, 4, 0);
 
         } break;
 
@@ -338,8 +336,8 @@ class VideoBasedHMDTracker : boost::noncopyable {
             m_frame = m_images[m_currentImage++];
         }
 
-        // Sleep 1/120th of a second, to simulate a reasonable
-        // frame rate.
+// Sleep 1/120th of a second, to simulate a reasonable
+// frame rate.
 //        vrpn_SleepMsecs(1000 / 120);
 #else
         if (!m_camera.isOpened()) {
@@ -348,9 +346,9 @@ class VideoBasedHMDTracker : boost::noncopyable {
             return OSVR_RETURN_SUCCESS;
         }
 
-        //==================================================================
-        // Trigger a camera grab.  Pull it into an OpenCV matrix named
-        // m_frame.
+//==================================================================
+// Trigger a camera grab.  Pull it into an OpenCV matrix named
+// m_frame.
 #ifdef VBHMD_USE_DIRECTSHOW
         if (!m_camera.read_image_to_memory()) {
             // Couldn't open the camera.  Failing silently for now. Maybe the
@@ -358,10 +356,11 @@ class VideoBasedHMDTracker : boost::noncopyable {
             return OSVR_RETURN_SUCCESS;
         }
         int minx, miny, maxx, maxy;
-        m_camera.read_range(minx, maxx, miny,maxy);
-        int height = maxy - miny+1;
-        int width = maxx - minx+1;
-        m_frame = cv::Mat(height, width, CV_8UC3, (BYTE*)(m_camera.get_pixel_buffer_pointer()));
+        m_camera.read_range(minx, maxx, miny, maxy);
+        int height = maxy - miny + 1;
+        int width = maxx - minx + 1;
+        m_frame = cv::Mat(height, width, CV_8UC3,
+                          (BYTE *)(m_camera.get_pixel_buffer_pointer()));
 
         //==================================================================
         // Flip the image in Y to take it from DirectShow space into
@@ -396,7 +395,7 @@ class VideoBasedHMDTracker : boost::noncopyable {
 #ifdef VBHMD_TIMING
         //==================================================================
         // Time our performance
-        static struct timeval last = { 0, 0 };
+        static struct timeval last = {0, 0};
         if (last.tv_sec == 0) {
             vrpn_gettimeofday(&last, NULL);
         }
@@ -405,8 +404,8 @@ class VideoBasedHMDTracker : boost::noncopyable {
             struct timeval now;
             vrpn_gettimeofday(&now, NULL);
             double duration = vrpn_TimevalDurationSeconds(now, last);
-            std::cout << "Video-based tracker: update rate "
-                << count/duration << " hz" << std::endl;
+            std::cout << "Video-based tracker: update rate " << count / duration
+                      << " hz" << std::endl;
             count = 0;
             last = now;
         }
