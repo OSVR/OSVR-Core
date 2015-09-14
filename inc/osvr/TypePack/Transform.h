976e8e16518dfe5398a126fe29e0f04e04ead4ea
Ryan Pavlik <ryan@sensics.com>
2015-09-14 12:20:22 -0500

    Clean up and fix transform.

--- inc/osvr/TypePack/Transform.h
+++ inc/osvr/TypePack/Transform.h
@@ -21,7 +21,8 @@
 
 // Internal Includes
 #include "Apply.h"
-#include "CoerceList.h"
+#include "List.h"
+#include "T.h"
 
 // Library/third-party includes
 // - none
@@ -34,16 +35,21 @@ namespace typepack {
 
     namespace detail {
 
-        template <typename, typename> struct transform_ {};
+        /// The trait used to implement the alias typepack::transform
+        template <typename List, typename Fun> struct transform_;
         template <typename... Ts, typename Fun>
         struct transform_<list<Ts...>, Fun> {
             /// required for MSVC2013 to avoid "there are no parameter packs
             /// available to expand"
-            template <typename T>
-            struct apply_workaround : apply<Fun, T>::type {};
-            typedef coerce_list<typename apply_workaround<Ts>::type...> type;
+            template <typename T> struct apply_workaround {
+                using type = typepack::apply<Fun, T>;
+            };
+            using type = list<typename apply_workaround<Ts>::type...>;
         };
     } // namespace detail
+
+    /// Given a list and an alias class, apply the alias class to each element
+    /// in the list and return the results in a list.
     template <typename List, typename Fun>
     using transform = t_<detail::transform_<List, Fun>>;
 } // namespace typepack
