2af169e703cb6e221832b864d420bb52bff0a6b8
Ryan Pavlik <ryan@sensics.com>
2015-09-22 17:27:14 -0500

    Initial handling of being a subproject - turn off examples and header tests.

--- cmake-local/osvrBuildOptions.cmake
+++ cmake-local/osvrBuildOptions.cmake
@@ -2,6 +2,17 @@
 # NOTE: To configure the build, don't edit this file - use CMake to set the options (in the GUI or on the command line)
 # These are just defaults, and are split into a separate file for easier forwarding from superbuilds.
 ###
+if(COMMAND get_subproject_status)
+    get_subproject_status(OSVR_SUBPROJECT)
+else()
+    set(OSVR_SUBPROJECT NO)
+endif()
+
+if(OSVR_SUBPROJECT)
+    set(OSVR_ON_UNLESS_SUBPROJECT OFF)
+else()
+    set(OSVR_ON_UNLESS_SUBPROJECT ON)
+endif()
 
 ###
 # Configuration Options
@@ -14,15 +25,15 @@ option(BUILD_SHARED_LIBS "Should we build as a shared library?" ON)
 option(BUILD_DEV_VERBOSE "Build with verbose development messages?" ON)
 
 option(BUILD_SERVER "Should we build the server libraries and required dev tools?" ON)
-option(BUILD_SERVER_EXAMPLES "Should we build the server-related example apps and plugins?" ON)
+option(BUILD_SERVER_EXAMPLES "Should we build the server-related example apps and plugins?" ${OSVR_ON_UNLESS_SUBPROJECT})
 option(BUILD_SERVER_APP "Should we build the server app?" ON)
 option(BUILD_SERVER_PLUGINS "Should we build the standard plugins?" ON)
 
 option(BUILD_CLIENT "Should we build the client libraries?" ON)
-option(BUILD_CLIENT_EXAMPLES "Should we build the client example apps?" ON)
+option(BUILD_CLIENT_EXAMPLES "Should we build the client example apps?" ${OSVR_ON_UNLESS_SUBPROJECT})
 option(BUILD_CLIENT_APPS "Should we build the client apps?" ON)
 
-option(BUILD_HEADER_DEPENDENCY_TESTS "Should we add targets to ensure that every public header compiles cleanly on its own? Increases number of targets greatly..." ON)
+option(BUILD_HEADER_DEPENDENCY_TESTS "Should we add targets to ensure that every public header compiles cleanly on its own? Increases number of targets greatly..." ${OSVR_ON_UNLESS_SUBPROJECT})
 
 if(NOT BUILD_CLIENT AND NOT BUILD_SERVER)
     message(FATAL_ERROR "Must have at least one of BUILD_CLIENT and BUILD_SERVER enabled!")
@@ -45,4 +56,3 @@ foreach(dependent_option ${dependent_options})
         message(STATUS "Disabling ${dependent_option}: depends on ${dependency}, which is disabled")
     endif()
 endforeach()
-
