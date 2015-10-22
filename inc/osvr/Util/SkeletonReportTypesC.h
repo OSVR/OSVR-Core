0249fa2cea210b2cd38c0ccd3ddfd5bba66cc307
Georgiy Frolov <gfrolov@sensics.com>
2015-10-22 16:00:33 -0400

    update skeleton report types ensure connectedness of reports

--- inc/osvr/Util/SkeletonReportTypesC.h
+++ inc/osvr/Util/SkeletonReportTypesC.h
@@ -42,6 +42,7 @@ OSVR_EXTERN_C_BEGIN
     @{
 */
 
+/** @brief defines the elements that are available in skeleton as per OSVR Skeleton Spec. */
 typedef enum OSVR_SkeletonElements{
     OSVR_SKELETON_PELVIS,
     OSVR_SKELETON_SPINE0,
@@ -104,22 +105,18 @@ typedef enum OSVR_SkeletonElements{
     OSVR_SKELETON_TOES_RIGHT
 } OSVR_SkeletonElements;
 
-/** @brief There are various types of skeleton reports that allow to get different skeleton joints/bones. Refer to the definition of each report below for a complete description of which bones are included */
+/** @brief There are various types of skeleton reports that allow to get different skeleton joints/bones. Note, Each report can include information from one skeleton sensor due to connectedness of skeleton. Refer to the definition of each report below for a complete description of which bones are included */
 typedef enum OSVR_SkeletonReportSizes {
     OSVR_SKELETON_REP_HEAD_ = 2,
     OSVR_SKELETON_REP_ARM = 19,
-    OSVR_SKELETON_REP_ARMS = 38,
     OSVR_SKELETON_REP_LEG = 4,
-    OSVR_SKELETON_REP_LEGS = 8,
     OSVR_SKELETON_REP_FOOT = 4,
-    OSVR_SKELETON_REP_FEET = 2,
     OSVR_SKELETON_REP_HAND = 16,
-    OSVR_SKELETON_REP_HANDS = 34,
-    OSVR_SKELETON_REP_LOA1 = 15
+    OSVR_SKELETON_REP_LOA1 = 19,
     OSVR_SKELETON_REP_LOA2 = 53
 } OSVR_SkeletonReportSizes;
 
-/** @brief A state of a skeleton element (joint/bone) */
+/** @brief A state of a single skeleton element (joint/bone) */
 typedef struct OSVR_SkeletonElementReport{
     /** @brief A skeleton element ID that specifies which bone/joint it is. May not be valid if joint not */
     uint32_t element;
@@ -131,15 +128,13 @@ typedef struct OSVR_SkeletonElementReport{
 
 /** @brief Report for Skeleton Level of Articulation 1 (LOA1)
     As defined in H-Anim Low Level of Articulation provides a scaled down version of skeleton joints and includes the following:
-    Head, Clavicle Left/Right (Shoulders), Arm Upper Left/Right (Elbows), Hand Left/Right (Wrists), Spine1 (Center spine), Pelvis, Leg Upper Left/Right (Hips), Leg Lower Left/Right (Knees), Foot Left/Right
-    
+    Head, Neck, Clavicle Left/Right (Shoulders), Arm Upper Left/Right (Elbows), Hand Left/Right (Wrists), Spine 0/1/2/3 (Center spine), Pelvis, Leg Upper Left/Right (Hips), Leg Lower Left/Right (Knees), Foot Left/Right
 */ 
 typedef struct OSVR_SkeletonLOA1Report{
     uint32_t sensor;
     /** @brief a collection of tracker reports */
-    OSVR_PoseReport states[OSVR_SKELETON_LOA1];
-}
-
+    OSVR_PoseReport states[OSVR_SKELETON_REP_LOA1];
+} OSVR_SkeletonLOA1Report;
 
 /** @brief Report for Skeleton Level of Articulation 2 (LOA2)
     H-Anim defines  a humanoid figure with 72 joints to have high Level of articulation however OSVR Skeleton Interface defines a total of 53 joints/bones and it includes all elements described above:
@@ -153,18 +148,8 @@ typedef struct OSVR_SkeletonLOA2Report {
     OSVR_SkeletonState states[OSVR_SKELETON_REP_LOA2];
 } OSVR_SkeletonLOA2Report;
 
-/** @brief Report for Skeleton Hands (Left and Right)
-    The report includes for Left/Right: Lower Hand, Hand, Thumb/Index/Middle/Ring/Pinky Proximal/Medial/Distal
-*/ 
-typedef struct OSVR_SkeletonHandsReport {
-    /** @brief A skeleton interface sensor ID */
-    uint32_t sensor;
-    /** @brief A collection of skeleton element reports that only contains the above elements */
-    OSVR_SkeletonElementReport states[OSVR_SKELETON_REP_HANDS];
-} OSVR_SkeletonHandsReport;
-
 /** @brief Report for One Hand (Left or Right)
-    Each hand report includes : Lower Hand, Hand, Thumb/Index/Middle/Ring/Pinky Proximal/Medial/Distal
+    Each hand report includes : Hand (Wrist), Thumb/Index/Middle/Ring/Pinky Proximal/Medial/Distal
 */ 
 typedef struct OSVR_SkeletonHandReport {
     /** @brief A skeleton interface sensor ID */
@@ -173,25 +158,36 @@ typedef struct OSVR_SkeletonHandReport {
     OSVR_SkeletonElementReport states[OSVR_SKELETON_REP_HAND];
 } OSVR_SkeletonHandReport;
 
-/** @brief Report for both feet (Left and Right)
-    The report includes the following elements: Left/Right Foot, Left/Right Toes
+/** @brief Report for a single Arm (Left or Right)
+    Each hand report includes : Clavicle, Arm Upper, Arm Lower, Hand, Thumb/Index/Middle/Ring/Pinky Proximal/Medial/Distal
 */ 
-typedef struct OSVR_SkeletonFeetReport {
+typedef struct OSVR_SkeletonArmReport {
     /** @brief A skeleton interface sensor ID */
     uint32_t sensor;
     /** @brief A collection of skeleton element reports that only contains the above elements */
-    OSVR_SkeletonElementReport states[OSVR_SKELETON_REP_FEET];
-} OSVR_SkeletonFeetReport;
+    OSVR_SkeletonElementReport states[OSVR_SKELETON_REP_ARM];
+} OSVR_SkeletonArmReport;
 
-/** @brief Report for both feet (Left and Right)
-    The report includes the following elements: Left/Right Foot, Left/Right Toes
+/** @brief Report for a single foot
+    The report includes the following elements: Foot, Toes
 */ 
-typedef struct OSVR_SkeletonFeetReport {
+typedef struct OSVR_SkeletonFootReport {
     /** @brief A skeleton interface sensor ID */
     uint32_t sensor;
     /** @brief A collection of skeleton element reports that only contains the above elements */
-    OSVR_SkeletonElementReport states[OSVR_SKELETON_REP_FEET];
-} OSVR_SkeletonFeetReport;
+    OSVR_SkeletonElementReport states[OSVR_SKELETON_REP_FOOT];
+} OSVR_SkeletonFootReport;
+
+/** @brief Report for a single leg
+    The report includes the following elements: Leg Lower, Leg Upper, Foot, Toes 
+*/ 
+typedef struct OSVR_SkeletonLegReport {
+    /** @brief A skeleton interface sensor ID */
+    uint32_t sensor;
+    /** @brief A collection of skeleton element reports that only contains the above elements */
+    OSVR_SkeletonElementReport states[OSVR_SKELETON_REP_LEG];
+} OSVR_SkeletonLegReport;
+
 /** @} */
 
 OSVR_EXTERN_C_END
