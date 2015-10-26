92136e3fd2e4854ec4d24a19d210bf61485df6b0
Georgiy Frolov <gfrolov@sensics.com>
2015-10-26 13:51:37 -0400

    add a sample skeleton client app

---
+++ examples/clients/Skeleton.c
@@ -0,0 +1,53 @@
+/** @file
+    @brief Implementation
+
+    @date 2015
+
+    @author
+    Sensics, Inc.
+    <http://sensics.com/osvr>
+*/
+
+// Copyright 2015 Sensics, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Internal Includes
+#include <osvr/ClientKit/ContextC.h>
+#include <osvr/ClientKit/InterfaceC.h>
+#include <osvr/ClientKit/InterfaceStateC.h>
+
+// Library/third-party includes
+
+// Standard includes
+#include <stdio.h>
+
+int main() {
+    OSVR_ClientContext ctx = osvrClientInit("com.osvr.exampleclients.SkeletonCallback", 0);
+
+    OSVR_ClientInterface leftHand = NULL;
+    OSVR_ClientInterface rightHand = NULL;    
+    
+    osvrClientGetInterface(ctx, "/me/hands/left", &leftHand);
+    osvrClientGetInterface(ctx, "/me/hands/right", &rightHand);
+
+    // Pretend that this is your application's mainloop.
+    while (1) {
+        osvrClientUpdate(ctx);
+        //get some skeleton Reports
+    }
+
+    osvrClientShutdown(ctx);
+    printf("Library shut down, exiting.\n");
+    return 0;
+}
---
+++ examples/clients/Skeleton.cpp
@@ -0,0 +1,52 @@
+/** @file
+    @brief Implementation
+
+    @date 2015
+
+    @author
+    Sensics, Inc.
+    <http://sensics.com/osvr>
+*/
+
+// Copyright 2015 Sensics, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Internal Includes
+#include <osvr/ClientKit/Context.h>
+#include <osvr/ClientKit/Interface.h>
+#include <osvr/Util/ClientReportTypesC.h>
+
+// Library/third-party includes
+
+// Standard includes
+#include <iostream>
+#include <string>
+
+int main() {
+    osvr::clientkit::ClientContext context(
+        "com.osvr.exampleclients.SkeletonCallback");
+
+    osvr::clientkit::Interface skeleton = context.getInterface(
+        "/com_osvr_example_Skeleton/Skeleton/skeleton");
+
+    //eyetracker.registerCallback(&eyeTracker2DCallback, NULL);
+
+    // Pretend that this is your application's mainloop.
+    while (1) {
+        context.update();
+    }
+
+    std::cout << "Library shut down, exiting." << std::endl;
+    return 0;
+}
