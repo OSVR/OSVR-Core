e31e06fa56ba6f08064abdd6a4779bdc6109727c
Jules Blok <jules.blok@gmail.com>
2015-11-04 17:52:34 +0100

    Util: Add header for aligned memory allocation.

--- src/osvr/Common/ImagingComponent.cpp
+++ src/osvr/Common/ImagingComponent.cpp
@@ -27,6 +27,7 @@
 #include <osvr/Common/BaseDevice.h>
 #include <osvr/Common/Serialization.h>
 #include <osvr/Common/Buffer.h>
+#include <osvr/Util/AlignedMemory.h>
 #include <osvr/Util/Flag.h>
 #include <osvr/Util/Verbosity.h>
 
@@ -34,8 +35,6 @@
 #include <stdlib.h>
 #include <sstream>
 #include <utility>
-
-#define OSVR_ALIGN_SIZE 16
 
 namespace osvr {
 namespace common {
@@ -70,30 +69,12 @@ namespace common {
 
             template <typename T>
             void allocateBuffer(T &, size_t bytes, std::true_type const &) {
-                // Allocate a memory buffer with enough space to store a pointer to the
-                // original buffer.
-                size_t space = bytes + OSVR_ALIGN_SIZE;
-                void* buffer = malloc(space + sizeof(void*));
-                void* aligned = (void**)buffer + 1;
-
-                // After this call the 'aligned' pointer will be aligned to a boundary.
-                // If there is not enough space to align the pointer, it stays unaligned.
-                std::align(OSVR_ALIGN_SIZE, bytes, aligned, space);
-
-                // Store the buffer pointer for the delete call.
-                ((void **)aligned)[-1] = buffer;
-
-                m_imgBuf.reset(reinterpret_cast<OSVR_ImageBufferElement *>(aligned), deleteBuffer);
+                m_imgBuf.reset(reinterpret_cast<OSVR_ImageBufferElement *>(util::aligned_alloc(bytes)), util::aligned_free);
             }
 
             template <typename T>
             void allocateBuffer(T &, size_t, std::false_type const &) {
                 // Does nothing if we're serializing.
-            }
-
-            static void deleteBuffer(void* p) {
-                if (p)
-                    free(((void **)p)[-1]);
             }
 
             template <typename T> void processMessage(T &p) {
