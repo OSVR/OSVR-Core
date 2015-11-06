62f6b8d5771da70b79c75f45616a69dc94cccd3a
Ryan Pavlik <ryan@sensics.com>
2015-11-06 16:34:15 -0600

    Add build option for OpenCV usage.
    Now there's a BUILD_WITH_OPENCV option to switch on/off the projects
    that require OpenCV.

--- cmake-local/osvrBuildOptions.cmake
+++ cmake-local/osvrBuildOptions.cmake
@@ -19,7 +19,7 @@ endif()
 ###
 
 # List of options, for easier consumption by superbuilds. Leaving off the BUILD_ prefix
-set(OSVR_BUILD_OPTIONS SERVER SERVER_EXAMPLES SERVER_APP SERVER_PLUGINS CLIENT CLIENT_EXAMPLES CLIENT_APPS)
+set(OSVR_BUILD_OPTIONS SERVER SERVER_EXAMPLES SERVER_APP SERVER_PLUGINS CLIENT CLIENT_EXAMPLES CLIENT_APPS WITH_OPENCV)
 
 option(BUILD_SHARED_LIBS "Should we build as a shared library?" ON)
 option(BUILD_DEV_VERBOSE "Build with verbose development messages?" ON)
@@ -32,6 +32,8 @@ option(BUILD_SERVER_PLUGINS "Should we build the standard plugins?" ON)
 option(BUILD_CLIENT "Should we build the client libraries?" ON)
 option(BUILD_CLIENT_EXAMPLES "Should we build the client example apps?" ${OSVR_ON_UNLESS_SUBPROJECT})
 option(BUILD_CLIENT_APPS "Should we build the client apps?" ON)
+
+option(BUILD_WITH_OPENCV "Should we build the stock and example plugins and apps (as otherwise configured) that require OpenCV?" ${OSVR_ON_UNLESS_SUBPROJECT})
 
 option(BUILD_HEADER_DEPENDENCY_TESTS "Should we add targets to ensure that every public header compiles cleanly on its own? Increases number of targets greatly..." ${OSVR_ON_UNLESS_SUBPROJECT})
 
