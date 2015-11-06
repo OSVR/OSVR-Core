7bab9c01193bbec6a3989c59eb48bafbc29daa60
Ryan Pavlik <ryan@sensics.com>
2015-11-05 18:04:47 -0600

    Some tweaks to the aligned memory header.

--- inc/osvr/Util/AlignedMemory.h
+++ inc/osvr/Util/AlignedMemory.h
@@ -37,24 +37,29 @@
 #include <stdlib.h>
 #include <memory>
 
-#define OSVR_ALIGN_SIZE 16
-
 namespace osvr {
 namespace util {
+    enum {
+        /// The default (and core-utilized) alignment of imaging buffers, etc.
+        OSVR_DEFAULT_ALIGN_SIZE = 16
+    };
+
     /// @brief Aligned allocation function, gives a pointer to a block of
     /// memory aligned to a memory boundary.
-    ///
-    /// Used in header-only C++ wrappers over C APIs.
-    inline void* aligned_alloc(size_t bytes) {
+    inline void *aligned_alloc(size_t bytes,
+                               size_t alignment = OSVR_DEFAULT_ALIGN_SIZE) {
         // Allocate a memory buffer with enough space to store a pointer to the
         // original buffer.
-        size_t space = bytes + OSVR_ALIGN_SIZE;
-        void* buffer = malloc(space + sizeof(void*));
-        void* aligned = (void**)buffer + 1;
+        const auto space = bytes + alignment + sizeof(void *);
+        void *buffer = malloc(space);
+
+        // Reserve the first byte for our buffer pointer for later freeing.
+        void *aligned = (void **)buffer + 1;
 
         // After this call the 'aligned' pointer will be aligned to a boundary.
-        // If there is not enough space to align the pointer, it stays unaligned.
-        std::align(OSVR_ALIGN_SIZE, bytes, aligned, space);
+        // If there is not enough space to align the pointer, it stays
+        // unaligned.
+        std::align(alignment, bytes, aligned, space);
 
         // Store the buffer pointer for the delete call.
         ((void **)aligned)[-1] = buffer;
@@ -64,12 +69,11 @@ namespace util {
 
     /// @brief Aligned deallocation function, uses the pointer to the original
     /// memory block to deallocate it.
-    ///
-    /// Used in header-only C++ wrappers over C APIs.
-    inline void aligned_free(void* p) {
+    inline void aligned_free(void *p) {
         // Null-pointer should be a no-op.
-        if (p)
+        if (p) {
             free(((void **)p)[-1]);
+        }
     }
 } // namespace util
 } // namespace osvr
