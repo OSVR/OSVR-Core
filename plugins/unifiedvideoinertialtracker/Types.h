75b46297493a5b99d2d62588635d2124d949a583
Ryan Pavlik <ryan@sensics.com>
2016-02-10 09:40:10 -0600

    Style

--- plugins/unifiedvideoinertialtracker/Types.h
+++ plugins/unifiedvideoinertialtracker/Types.h
@@ -43,10 +43,10 @@ namespace vbtracker {
 /// @todo Remove when we no longer assume a single camera or monotonic camera
 /// timestamps, and the build will break in a few places
 /// where known "gotchas" exist
-    #define OSVR_UVBI_ASSUME_SINGLE_CAMERA
-    /// @todo Remove when we no longer assume that IMU reports arrive before video reports with same timestamps.
-    #define OSVR_UVBI_ASSUME_CAMERA_ALWAYS_SLOWER
-
+#define OSVR_UVBI_ASSUME_SINGLE_CAMERA
+/// @todo Remove when we no longer assume that IMU reports arrive before video
+/// reports with same timestamps.
+#define OSVR_UVBI_ASSUME_CAMERA_ALWAYS_SLOWER
 
     class Led;
     class LedIdentifier;
