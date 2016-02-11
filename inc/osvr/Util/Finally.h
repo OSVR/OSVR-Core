1c705b05986237002e03124d7384012d6184fb55
Ryan Pavlik <ryan@sensics.com>
2016-02-11 12:49:08 -0600

    Improve "finally" comments.

--- inc/osvr/Util/Finally.h
+++ inc/osvr/Util/Finally.h
@@ -1,6 +1,15 @@
 /** @file
     @brief Header declaring a C++11 `finally` or "scope-guard" construct.
 
+    Inspirations are many - Alexandrescu's original and C++11 ScopeGuard, and
+    the Guideline Support Library's (GSL) `final_act`/`finally`
+    https://github.com/Microsoft/GSL/blob/0cf947db7760bf5756e4cb0d47c72a257ed527c5/include/gsl_util.h
+    come to mind, but this has been written and re-written numerous times
+    since its introduction into the C++ global consciousness, and this
+    implementation was written independently after I couldn't find a
+    previous independent implementation I had written a few weeks earlier in
+    an implementation file. -- Ryan Pavlik
+
     @date 2016
 
     @author
@@ -36,8 +45,12 @@
 
 namespace osvr {
 namespace util {
+    /// Allows you to run a callable something at the end of a scope.
+    ///
     /// The class that provides the scope-guard behavior. Often not referred to
-    /// by name because auto is useful here.
+    /// by name because auto is useful here, and often not created by a direct
+    /// constructor call because of the finally() convenience functions combined
+    /// with lambdas.
     template <typename F> class FinalTask {
       public:
         /// Explicit constructor from something callable.
@@ -66,7 +79,9 @@ namespace util {
         bool do_ = true;
     };
 
-    /// Creation free function for final tasks. Use like:
+    /// Creation free function for final tasks to run on scope exit. Works great
+    /// when paired with lambdas (particularly with `[&]` reference capture).
+    /// Use like:
     /// `auto f = finally([&]{ dothis(); });` to have `dothis()` called when `f`
     /// goes out of scope, no matter how.
     template <typename F> inline auto finally(F &&f) -> FinalTask<F> {
