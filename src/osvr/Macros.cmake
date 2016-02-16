74bfa91c6c70b4c25837bd1fb1f746fcf965a9b4
Georgiy Frolov <gfrolov@sensics.com>
2016-02-16 17:02:22 -0500

    replace generate_export_header with our own header template

--- src/osvr/Macros.cmake
+++ src/osvr/Macros.cmake
@@ -1,6 +1,11 @@
 set(LIB_FOLDER "OSVR Libraries")
 
 set(OSVR_BUILDTREE_TARGETS "" CACHE INTERNAL "" FORCE)
+
+# Get a path to this file
+get_filename_component(_MACROS_DIR 
+    "${CMAKE_CURRENT_LIST_FILE}" PATH)
+    
 macro(osvr_append_target _targetlist _target)
     list(APPEND OSVR_${_targetlist}_TARGETS "${_target}")
     set(OSVR_${_targetlist}_TARGETS "${OSVR_${_targetlist}_TARGETS}" CACHE INTERNAL "" FORCE)
@@ -34,10 +39,19 @@ macro(osvr_add_library)
 
     set_target_properties(${LIBNAME_FULL} PROPERTIES
         FOLDER "${LIB_FOLDER}")
-
-    generate_export_header(${LIBNAME_FULL}
-        BASE_NAME ${EXPORT_BASENAME}
-        EXPORT_FILE_NAME Export.h)
+        
+    # Are we building or using this library?
+    get_target_property(EXPORT_IMPORT_CONDITION ${LIBNAME_FULL} DEFINE_SYMBOL)   
+    
+    # If we are building then set export condition
+    if(NOT EXPORT_IMPORT_CONDITION)
+        set(EXPORT_IMPORT_CONDITION ${LIBNAME_FULL}_EXPORTS)
+    endif()
+    string(MAKE_C_IDENTIFIER ${EXPORT_IMPORT_CONDITION} EXPORT_IMPORT_CONDITION)
+                            
+    configure_file("${_MACROS_DIR}/exportheader.cmake.in"
+        Export.h
+        @ONLY NEWLINE_STYLE LF)
 
     if(NOT BUILD_SHARED_LIBS)
         target_compile_definitions(${LIBNAME_FULL} PUBLIC ${EXPORT_BASENAME}_STATIC_DEFINE)
