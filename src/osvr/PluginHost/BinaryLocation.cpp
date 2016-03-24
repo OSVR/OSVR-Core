56d6f82b5c170263c6f6e25d7001d53775a77238
Ryan Pavlik <ryan@sensics.com>
2016-03-24 15:45:30 -0500

    Make sure we're getting the non-wide filename.

--- src/osvr/PluginHost/BinaryLocation.cpp
+++ src/osvr/PluginHost/BinaryLocation.cpp
@@ -51,7 +51,7 @@ namespace pluginhost {
 #if defined(OSVR_WINDOWS)
     std::string getBinaryLocation() {
         char buf[512] = {0};
-        DWORD len = GetModuleFileName(nullptr, buf, sizeof(buf));
+        DWORD len = GetModuleFileNameA(nullptr, buf, sizeof(buf));
         std::string ret;
         if (0 != len) {
             ret.assign(buf, len);
