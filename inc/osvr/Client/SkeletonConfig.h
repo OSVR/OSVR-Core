163ef343d96d6a09de0cad3ad3558e8f899983f2
Georgiy Frolov <gfrolov@sensics.com>
2016-04-12 15:57:20 -0400

    add skeletonConfig to handle internals of Skeleton's ClientKit methods

---
+++ inc/osvr/Client/SkeletonConfig.h
@@ -0,0 +1,205 @@
+/** @file
+    @brief Header
+
+    @date 2016
+
+    @author
+    Sensics, Inc.
+    <http://sensics.com/osvr>
+*/
+
+// Copyright 2016 Sensics, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef INCLUDED_SkeletonConfig_h_GUID_A1D6BCE7_D044_4582_9BA5_860C790A3013
+#define INCLUDED_SkeletonConfig_h_GUID_A1D6BCE7_D044_4582_9BA5_860C790A3013
+
+// Internal Includes
+#include <osvr/Client/Export.h>
+#include <osvr/Client/DisplayInput.h>
+#include <osvr/Client/Viewer.h>
+#include <osvr/Client/Viewers.h>
+#include <osvr/Client/InternalInterfaceOwner.h>
+#include <osvr/Common/ClientInterface.h>
+#include <osvr/Common/PathTree.h>
+#include <osvr/Common/RegisteredStringMap.h>
+#include <osvr/Common/ClientInterfacePtr.h>
+#include <osvr/Util/ContainerWrapper.h>
+#include <osvr/Util/Pose3C.h>
+#include <osvr/Util/TimeValue.h>
+#include <osvr/Util/ClientReportTypesC.h>
+#include <osvr/Util/ClientOpaqueTypesC.h>
+#include <osvr/Util/ChannelCountC.h>
+#include <osvr/Util/UniquePtr.h>
+
+// Library/third-party includes
+// - none
+
+// Standard includes
+#include <vector>
+
+namespace osvr {
+namespace client {
+
+    typedef std::vector<std::pair<util::StringID, common::ClientInterfacePtr>>
+        InterfaceMap;
+
+    struct NoCtxYet : std::runtime_error {
+        NoCtxYet()
+            : std::runtime_error("Client context is not yet initialized!") {}
+    };
+
+    struct IdNotFound : std::runtime_error {
+        IdNotFound()
+            : std::runtime_error(
+                  "Could not find joint/bone Id with provided info!") {}
+    };
+
+    class SkeletonConfig;
+    typedef unique_ptr<SkeletonConfig> SkeletonConfigPtr;
+    class SkeletonConfigFactory {
+      public:
+        OSVR_CLIENT_EXPORT static SkeletonConfigPtr
+        create(OSVR_ClientContext ctx, OSVR_ClientInterface iface);
+    };
+
+    class SkeletonConfig {
+      public:
+        // non-copyable
+        SkeletonConfig(SkeletonConfig const &) = delete;
+        SkeletonConfig &operator=(SkeletonConfig const &) = delete;
+
+        bool getBoneId(const char *boneName, OSVR_SkeletonBoneCount *boneId);
+        bool getJointId(const char *jointName,
+                        OSVR_SkeletonJointCount *jointId);
+
+        std::string const getBoneName(OSVR_SkeletonBoneCount boneId);
+        std::string const getJointName(OSVR_SkeletonJointCount jointId);
+
+        OSVR_Pose3 getBoneState(OSVR_SkeletonBoneCount boneId);
+        OSVR_Pose3 getJointState(OSVR_SkeletonJointCount jointId);
+
+        OSVR_SkeletonBoneCount getNumBones();
+        OSVR_SkeletonJointCount getNumJoints();
+
+      private:
+        friend class SkeletonConfigFactory;
+        SkeletonConfig(OSVR_ClientContext ctx, OSVR_ClientInterface iface);
+        /// @brief check if the articulation tree has been updated, then we need
+        /// to traverse the articulation tree again, and update values
+        bool isSkeletonTreeUpdated() const;
+        OSVR_ClientContext m_ctx;
+        OSVR_ClientInterface m_iface;
+        osvr::common::PathTree m_articulationTree;
+        osvr::common::RegisteredStringMap m_jointMap;
+        osvr::common::RegisteredStringMap m_boneMap;
+        InterfaceMap m_jointInterfaces;
+        InterfaceMap m_boneInterfaces;
+    };
+
+    inline bool SkeletonConfig::getBoneId(const char *boneName,
+                                          OSVR_SkeletonBoneCount *boneId) {
+
+        osvr::util::StringID id = m_boneMap.getStringID(boneName);
+        if (id.empty()) {
+            return false;
+        } else {
+            *boneId = id.value();
+            return true;
+        }
+    }
+
+    inline bool SkeletonConfig::getJointId(const char *jointName,
+                                           OSVR_SkeletonBoneCount *jointId) {
+
+        osvr::util::StringID id = m_jointMap.getStringID(jointName);
+        if (id.empty()) {
+            return false;
+        } else {
+            *jointId = id.value();
+            return true;
+        }
+    }
+
+    inline OSVR_Pose3
+    SkeletonConfig::getJointState(OSVR_SkeletonJointCount jointId) {
+
+        OSVR_TimeValue timestamp;
+        OSVR_Pose3 pose;
+
+        // find an interface for given jointId
+        for (auto val : m_jointInterfaces) {
+            if (val.first.value() == jointId) {
+                // try to get state, if available
+                bool hasState =
+                    val.second->getState<OSVR_PoseReport>(timestamp, pose);
+                if (!hasState) {
+                    throw NoPoseYet();
+                }
+            }
+        }
+
+        return pose;
+    }
+
+    inline OSVR_Pose3
+    SkeletonConfig::getBoneState(OSVR_SkeletonBoneCount boneId) {
+
+        OSVR_TimeValue timestamp;
+        OSVR_Pose3 pose;
+
+        // find an interface for given boneId
+        for (auto val : m_boneInterfaces) {
+            if (val.first.value() == boneId) {
+                // try to get state, if available
+                bool hasState =
+                    val.second->getState<OSVR_PoseReport>(timestamp, pose);
+                if (!hasState) {
+                    throw NoPoseYet();
+                }
+            }
+        }
+
+        return pose;
+    }
+
+    inline OSVR_SkeletonBoneCount SkeletonConfig::getNumBones() {
+        return m_boneMap.getEntries().size();
+    }
+
+    inline OSVR_SkeletonBoneCount SkeletonConfig::getNumJoints() {
+        return m_jointMap.getEntries().size();
+    }
+
+    std::string const
+    SkeletonConfig::getBoneName(OSVR_SkeletonBoneCount boneId) {
+        auto boneName = m_boneMap.getStringFromId(util::StringID(boneId));
+        if (boneName.empty()) {
+            throw IdNotFound();
+        }
+        return boneName;
+    }
+    std::string const
+    SkeletonConfig::getJointName(OSVR_SkeletonJointCount jointId) {
+        auto jointName = m_jointMap.getStringFromId(util::StringID(jointId));
+        if (jointName.empty()) {
+            throw IdNotFound();
+        }
+        return jointName;
+    }
+
+} // namespace client
+} // namespace osvr
+
+#endif // INCLUDED_SkeletonConfig_h_GUID_A1D6BCE7_D044_4582_9BA5_860C790A3013
---
+++ src/osvr/Client/SkeletonConfig.cpp
@@ -0,0 +1,185 @@
+/** @file
+    @brief Implementation
+
+    @date 2016
+
+    @author
+    Sensics, Inc.
+    <http://sensics.com/osvr>
+*/
+
+// Copyright 2016 Sensics, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Internal Includes
+#include <osvr/Client/SkeletonConfig.h>
+#include <osvr/Common/SkeletonComponent.h>
+#include <osvr/Common/SkeletonComponentPtr.h>
+#include <osvr/Common/PathTree.h>
+#include <osvr/Common/PathNode.h>
+#include <osvr/Common/PathElementTools.h>
+#include <osvr/Common/PathTreeFull.h>
+#include <osvr/Common/PathElementTypes.h>
+#include <osvr/Common/ProcessArticulationSpec.h>
+#include <osvr/Common/ApplyPathNodeVisitor.h>
+#include <osvr/Util/TreeTraversalVisitor.h>
+#include <osvr/Util/SharedPtr.h>
+#include <osvr/Util/Verbosity.h>
+#include <osvr/Util/TreeNode.h>
+
+// Library/third-party includes
+#include <boost/variant/get.hpp>
+#include <boost/algorithm/string/predicate.hpp>
+#include <boost/algorithm/string/erase.hpp>
+
+// Standard includes
+#include <string>
+
+namespace osvr {
+namespace client {
+
+    class ArticulationTreeTraverser : public boost::static_visitor<> {
+      public:
+        /// @brief Constructor
+        ArticulationTreeTraverser(osvr::common::RegisteredStringMap *jointMap,
+                                  osvr::common::RegisteredStringMap *boneMap,
+                                  InterfaceMap *jointInterfaces,
+                                  InterfaceMap *boneInterfaces,
+                                  OSVR_ClientContext &ctx)
+            : boost::static_visitor<>(), m_jointMap(jointMap),
+              m_boneMap(boneMap), m_jointInterfaces(jointInterfaces),
+              m_boneInterfaces(boneInterfaces), m_ctx(ctx) {}
+
+        /// @brief ignore null element
+        void operator()(osvr::common::PathNode const &,
+                        osvr::common::elements::NullElement const &) {}
+
+        void
+        operator()(osvr::common::PathNode const &node,
+                   osvr::common::elements::ArticulationElement const &elt) {
+
+            /// register joints
+            if (elt.getArticulationType() == "joint") {
+                /// register joint ID
+                util::StringID jointID =
+                    m_jointMap->getStringID(node.getName());
+                /// get an interface for tracker path
+                common::ClientInterfacePtr iface =
+                    m_ctx->getInterface(elt.getTrackerPath().c_str());
+                /// store the id, interface association
+                m_jointInterfaces->push_back(std::make_pair(jointID, iface));
+            }
+            /// register bones
+            else if (elt.getArticulationType() == "bone") {
+                /// register boneId
+                util::StringID boneId = m_boneMap->getStringID(node.getName());
+                /// get an interface for tracker path
+                common::ClientInterfacePtr iface =
+                    m_ctx->getInterface(elt.getTrackerPath().c_str());
+                /// store the id, interface association
+                m_boneInterfaces->push_back(std::make_pair(boneId, iface));
+            }
+            /// @todo Add additional type of articulation here, if needed
+        }
+
+        /// @brief Catch-all for other element types.
+        template <typename T>
+        void operator()(osvr::common::PathNode const &node, T const &elt) {}
+
+      private:
+        osvr::common::RegisteredStringMap *m_jointMap;
+        osvr::common::RegisteredStringMap *m_boneMap;
+        InterfaceMap *m_jointInterfaces;
+        InterfaceMap *m_boneInterfaces;
+        OSVR_ClientContext m_ctx;
+    };
+
+    class NodeToTrackerPathVisitor : public boost::static_visitor<std::string> {
+      public:
+        /// @brief Constructor
+        NodeToTrackerPathVisitor() : boost::static_visitor<std::string>() {}
+
+        std::string const
+        operator()(osvr::common::PathNode const &node,
+                   osvr::common::elements::ArticulationElement const &elt) {
+            return elt.getTrackerPath();
+        }
+
+        /// @brief Catch-all for other element types.
+        template <typename T>
+        std::string const operator()(osvr::common::PathNode const &node,
+                                     T const &elt) {
+            return std::string();
+        }
+    };
+
+    SkeletonConfigPtr
+    SkeletonConfigFactory::create(OSVR_ClientContext ctx,
+                                  OSVR_ClientInterface iface) {
+
+        if (ctx->getStatus()) {
+            try {
+                /// setup the path tree from the skeleton component
+                osvr::common::PathTree articulationTree;
+                osvr::common::clonePathTree(
+                    ctx->getArticulationTree(iface->getPath()),
+                    articulationTree);
+
+                /// Articulation tree is still empty (not receiveed yet)
+                /// Spin the wheels until we get there
+                if (!articulationTree.getRoot().hasChildren()) {
+                   /* OSVR_DEV_VERBOSE(
+                        "Have the articulation tree but it's empty!");*/
+                    return SkeletonConfigPtr{};
+                }
+
+                /// Client context is ready and articulation tree received
+                SkeletonConfigPtr cfg(new SkeletonConfig(ctx, iface));
+                /// get the path tree
+                osvr::common::clonePathTree(articulationTree,
+                                            cfg->m_articulationTree);
+
+                ArticulationTreeTraverser traverser(
+                    &cfg->m_jointMap, &cfg->m_boneMap, &cfg->m_jointInterfaces,
+                    &cfg->m_boneInterfaces, cfg->m_ctx);
+                osvr::util::traverseWith(
+                    articulationTree.getRoot(),
+                    [&traverser](osvr::common::PathNode const &node) {
+                        osvr::common::applyPathNodeVisitor(traverser, node);
+                    });
+
+                return cfg;
+            } catch (std::exception &e) {
+                OSVR_DEV_VERBOSE(
+                    "Error getting skeleton tree - exception :" << e.what());
+                return SkeletonConfigPtr{};
+            } catch (...) {
+                OSVR_DEV_VERBOSE(
+                    "Couldn't create a skeleton config internally! "
+                    "Unknown exception!");
+                return SkeletonConfigPtr{};
+            }
+        } else {
+            OSVR_DEV_VERBOSE("Couldn't create a skeleton config! "
+                             "Client context is not yet initialized!!");
+            return SkeletonConfigPtr{};
+        }
+    }
+
+    SkeletonConfig::SkeletonConfig(OSVR_ClientContext ctx,
+                                   OSVR_ClientInterface iface)
+        : m_ctx(ctx), m_iface(iface) {}
+
+} // namespace client
+} // namespace osvr
