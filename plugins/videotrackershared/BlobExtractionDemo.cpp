4605b866e2a61b0cfbee386c2a87c47892c33174
Ryan Pavlik <ryan@sensics.com>
2016-04-27 18:57:50 -0500

    Handle videos in the demo app

--- plugins/videotrackershared/BlobExtractionDemo.cpp
+++ plugins/videotrackershared/BlobExtractionDemo.cpp
@@ -36,23 +36,73 @@
 
 // Standard includes
 #include <cmath>
+#include <iomanip>
 #include <iostream>
+#include <sstream>
 
 namespace osvr {
 namespace vbtracker {
 
-    void showImage(std::string const &title, cv::Mat const &img) {
-        cv::namedWindow(title);
-        cv::imshow(title, img);
+    void showImage(std::string const &title, cv::Mat const &img,
+                   bool showImages = true) {
+        if (showImages) {
+            cv::namedWindow(title);
+            cv::imshow(title, img);
+        }
     }
 
-    void handleImage(std::string const &fn, bool pause) {
+    void handleImage(std::string const &fn, cv::Mat color, cv::Mat gray,
+                     bool pause, bool showImages = true) {
         BlobParams p;
         p.filterByCircularity = true;
         p.minCircularity = 0.75;
 
         /// Initial image loading
         std::cout << "Handling image " << fn << std::endl;
+
+        // showImage("Original", gray);
+        EdgeHoleBasedLedExtractor extractor;
+        extractor(gray, p, true);
+
+        /// Basic thresholding to reduce background noise
+        showImage("Cleaned", extractor.getThresholdedImage(), showImages);
+        cv::imwrite(fn + ".thresh.png", extractor.getThresholdedImage());
+
+        /// Edge detection
+        showImage("Edges", extractor.getEdgeDetectedImage(), showImages);
+        cv::imwrite(fn + ".edge.png", extractor.getEdgeDetectedImage());
+
+        showImage("Binarized Blurred Edges",
+                  extractor.getEdgeDetectedBinarizedImage(), showImages);
+
+        /// Extract beacons from the edge detection image
+
+        /// Produce a colored debug image where each accepted contour is
+        /// encircled with a different color.
+        cv::Mat highlightedContours =
+            drawColoredContours(gray, extractor.getContours());
+
+        /// Draw and label the rejected centers
+        for (auto &center : extractor.getRejectedCenters()) {
+            drawSubpixelPoint(highlightedContours, center, cv::Scalar(0, 0, 0),
+                              1.2);
+            std::ostringstream os;
+            os << center;
+            cv::putText(highlightedContours, os.str(),
+                        cv::Point(center) + cv::Point(1, 1),
+                        cv::FONT_HERSHEY_SIMPLEX, 0.3, cv::Scalar(0, 0, 255));
+        }
+
+        showImage("Selected contours", highlightedContours, showImages);
+        cv::imwrite(fn + ".contours.png", highlightedContours);
+        if (pause) {
+            cv::waitKey();
+        }
+    }
+    void handleImage(std::string const &fn, bool pause) {
+
+        /// Initial image loading
+        std::cout << "Loading image " << fn << std::endl;
         cv::Mat color = cv::imread(fn, cv::IMREAD_COLOR);
         if (!color.data) {
             std::cerr << "Could not load image!" << std::endl;
@@ -67,34 +117,44 @@ namespace vbtracker {
             return;
         }
         // showImage("Original", gray);
-        EdgeHoleBasedLedExtractor extractor;
-        extractor(gray, p, true);
-
-        /// Basic thresholding to reduce background noise
-        showImage("Cleaned", extractor.getThresholdedImage());
-        cv::imwrite(fn + ".thresh.png", extractor.getThresholdedImage());
-
-        /// Edge detection
-        showImage("Edges", extractor.getEdgeDetectedImage());
-        cv::imwrite(fn + ".edge.png", extractor.getEdgeDetectedImage());
-
-        /// Extract beacons from the edge detection image
-
-        /// Produce a colored debug image where each accepted contour is
-        /// encircled with a different color.
-        cv::Mat highlightedContours =
-            drawColoredContours(gray, extractor.getContours());
-        showImage("Selected contours", highlightedContours);
-        cv::imwrite(fn + ".contours.png", highlightedContours);
-        if (pause) {
-            cv::waitKey();
-        }
+        handleImage(fn, color, gray, pause);
     }
 } // namespace vbtracker
 } // namespace osvr
 
+void processAVI(std::string const &fn) {
+    cv::VideoCapture capture;
+    capture.open(fn);
+    if (!capture.isOpened()) {
+        std::cerr << "Could not open video file " << fn << std::endl;
+        return;
+    }
+    std::size_t i = 0;
+    cv::Mat frame;
+    cv::Mat frameGray;
+    capture >> frame;
+    while (capture.read(frame)) {
+        std::ostringstream os;
+        os << fn << "." << std::setw(4) << std::setfill('0') << i << ".png";
+        auto pngFileName = os.str();
+        cv::imwrite(pngFileName, frame);
+        cv::cvtColor(frame, frameGray, cv::COLOR_BGR2GRAY);
+        osvr::vbtracker::handleImage(pngFileName, frame, frameGray, false,
+                                     false);
+        i++;
+    }
+}
 int main(int argc, char *argv[]) {
     /// Don't stop before exiting if we've got multiple to process.
+    if (argc == 2) {
+        auto fn = std::string{argv[1]};
+        if (fn.find(".avi") != std::string::npos &&
+            fn.find(".avi.") == std::string::npos) {
+            /// has .avi but isn't a file extracted from an AVI.
+            processAVI(fn);
+            return 0;
+        }
+    }
     bool pause = (argc < 3);
     for (int arg = 1; arg < argc; ++arg) {
         osvr::vbtracker::handleImage(argv[arg], true);
