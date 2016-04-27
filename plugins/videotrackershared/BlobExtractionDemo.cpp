b1a322056afbf23d563278f113db0ec59c867ed0
Ryan Pavlik <ryan@sensics.com>
2016-04-27 16:45:14 -0500

    Remove the duplicate implementation now of the new blob detector.

--- plugins/videotrackershared/BlobExtractionDemo.cpp
+++ plugins/videotrackershared/BlobExtractionDemo.cpp
@@ -25,6 +25,7 @@
 // Internal Includes
 #include <BlobExtractor.h>
 #include <BlobParams.h>
+#include <EdgeHoleBasedLedExtractor.h>
 #include <OptionalStream.h>
 #include <cvUtils.h>
 
@@ -36,8 +37,6 @@
 // Standard includes
 #include <cmath>
 #include <iostream>
-
-#define OSVR_DEBUG_CONTOUR_CONDITIONS
 
 namespace osvr {
 namespace vbtracker {
@@ -68,113 +67,32 @@ namespace vbtracker {
             return;
         }
         // showImage("Original", gray);
+        EdgeHoleBasedLedExtractor extractor;
+        extractor(gray, p, true);
 
         /// Basic thresholding to reduce background noise
-        auto thresholdInfo = ImageThresholdInfo(ImageRangeInfo(gray), p);
-        cv::Mat thresholded;
-        cv::threshold(gray, thresholded, p.absoluteMinThreshold, 255,
-                      cv::THRESH_TOZERO);
-        showImage("Cleaned", thresholded);
-        cv::imwrite(fn + ".thresh.png", thresholded);
+        showImage("Cleaned", extractor.getThresholdedImage());
+        cv::imwrite(fn + ".thresh.png", extractor.getThresholdedImage());
 
         /// Edge detection
-        cv::Mat edge;
-        cv::Laplacian(thresholded, edge, CV_8U, 5);
-        cv::imwrite(fn + ".edge.png", edge);
-        showImage("Edges", edge);
+        showImage("Edges", extractor.getEdgeDetectedImage());
+        cv::imwrite(fn + ".edge.png", extractor.getEdgeDetectedImage());
 
         /// Extract beacons from the edge detection image
-        std::vector<ContourType> contours;
-        LedMeasurementVec measurements;
-
-        // turn the edge detection into a binary image.
-        cv::Mat edgeTemp = edge > 0;
-
-        // The lambda ("continuation") is called with each "hole" in the edge
-        // detection image, it's up to us what to do with the contour we're
-        // given. We examine it for suitability as an LED, and if it passes our
-        // checks, add a derived measurement to our measurement vector and the
-        // contour itself to our list of contours for debugging display.
-        consumeHolesOfConnectedComponents(edgeTemp, [&](ContourType &&contour) {
-            auto data = getBlobDataFromContour(contour);
-            auto debugStream = [] {
-#ifdef OSVR_DEBUG_CONTOUR_CONDITIONS
-                return outputIf(std::cout, true);
-#else
-				return outputIf(std::cout, false);
-#endif
-            };
-
-            debugStream() << "\nContour centered at " << data.center;
-            debugStream() << " - diameter: " << data.diameter;
-            debugStream() << " - area: " << data.area;
-            debugStream() << " - circularity: " << data.circularity;
-            debugStream() << " - bounding box size: " << data.bounds.size();
-            if (data.area < p.minArea) {
-                debugStream() << "Reject based on area: " << data.area << " < "
-                              << p.minArea << "\n";
-                return;
-            }
-
-            {
-                /// Check to see if we accidentally picked up a non-LED
-                /// stuck between a few bright ones.
-                cv::Mat patch;
-                cv::getRectSubPix(gray, cv::Size(1, 1),
-                                  cv::Point2f(data.center), patch);
-                auto centerPointValue = patch.at<unsigned char>(0, 0);
-                if (centerPointValue < p.absoluteMinThreshold) {
-                    debugStream() << "Reject based on center point value: "
-                                  << int(centerPointValue) << " < "
-                                  << p.absoluteMinThreshold << "\n";
-                    return;
-                }
-            }
-
-            if (p.filterByCircularity) {
-                if (data.circularity < p.minCircularity) {
-                    debugStream()
-                        << "Reject based on circularity: " << data.circularity
-                        << " < " << p.minCircularity << "\n";
-                    return;
-                }
-            }
-            if (p.filterByConvexity) {
-                auto convexity = getConvexity(contour, data.area);
-                debugStream() << " - convexity: " << convexity;
-                if (convexity < p.minConvexity) {
-                    debugStream() << "Reject based on convexity: " << convexity
-                                  << " < " << p.minConvexity << "\n";
-
-                    return;
-                }
-            }
-
-            debugStream() << "Accepted!\n";
-            {
-                auto newMeas = LedMeasurement(
-                    cv::Point2f(data.center), static_cast<float>(data.diameter),
-                    gray.size(), static_cast<float>(data.area));
-                newMeas.circularity = static_cast<float>(data.circularity);
-                newMeas.setBoundingBox(data.bounds);
-
-                measurements.emplace_back(std::move(newMeas));
-            }
-            contours.emplace_back(std::move(contour));
-        });
 
         /// Produce a colored debug image where each accepted contour is
         /// encircled with a different color.
-        cv::Mat highlightedContours = drawColoredContours(gray, contours);
+        cv::Mat highlightedContours =
+            drawColoredContours(gray, extractor.getContours());
         showImage("Selected contours", highlightedContours);
         cv::imwrite(fn + ".contours.png", highlightedContours);
         if (pause) {
             cv::waitKey();
         }
     }
-
 } // namespace vbtracker
 } // namespace osvr
+
 int main(int argc, char *argv[]) {
     /// Don't stop before exiting if we've got multiple to process.
     bool pause = (argc < 3);
