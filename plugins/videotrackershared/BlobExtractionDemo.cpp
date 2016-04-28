8aaa9d2aacae8110b76097fca84f4d50fac74acd
Ryan Pavlik <ryan@sensics.com>
2016-04-28 11:58:47 -0500

    Label the rejects (better)

--- plugins/videotrackershared/BlobExtractionDemo.cpp
+++ plugins/videotrackershared/BlobExtractionDemo.cpp
@@ -30,6 +30,7 @@
 #include <cvUtils.h>
 
 // Library/third-party includes
+#include <boost/algorithm/string/case_conv.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -38,6 +39,7 @@
 #include <cmath>
 #include <iomanip>
 #include <iostream>
+#include <random>
 #include <sstream>
 
 namespace osvr {
@@ -82,15 +84,77 @@ namespace vbtracker {
         cv::Mat highlightedContours =
             drawColoredContours(gray, extractor.getContours());
 
-        /// Draw and label the rejected centers
-        for (auto &center : extractor.getRejectedCenters()) {
-            drawSubpixelPoint(highlightedContours, center, cv::Scalar(0, 0, 0),
-                              1.2);
-            std::ostringstream os;
-            os << center;
-            cv::putText(highlightedContours, os.str(),
-                        cv::Point(center) + cv::Point(1, 1),
-                        cv::FONT_HERSHEY_SIMPLEX, 0.3, cv::Scalar(0, 0, 255));
+        // Figure out where to put the text - further along the ray
+        // that passes from the image center through the reject center, to try
+        // to get it "away from the crowd"
+        {
+            static const auto FONT_FACE = cv::FONT_HERSHEY_PLAIN;
+            static const auto FONT_SCALE = 0.8;
+            static const int FONT_THICKNESS = 1;
+
+            auto imageSize = highlightedContours.size();
+            auto imageCenter =
+                cv::Point2d(imageSize.width / 2, imageSize.height / 2);
+            std::random_device rd;
+            std::mt19937 mt(rd());
+            std::uniform_real_distribution<double> vectorScaleDistribution(1.1,
+                                                                           1.3);
+            auto computeTextLocation = [&](cv::Point2d rejectCenter,
+                                           std::string const &text) {
+                auto centerToRejectVec = rejectCenter - imageCenter;
+                auto textPoint =
+                    cv::Point(centerToRejectVec * vectorScaleDistribution(mt) +
+                              imageCenter);
+
+                /// OK, so that's where we'll center the text, now we have to
+                /// see how big the text is to offset it...
+                auto textSize = cv::getTextSize(text, FONT_FACE, FONT_SCALE,
+                                                FONT_THICKNESS, nullptr);
+
+                auto textOffset =
+                    cv::Point(textSize.width / 2, textSize.height / 2);
+                return textPoint - textOffset;
+            };
+
+            /// Draw and label the rejected centers of contours.
+            for (auto &reject : extractor.getRejectList()) {
+                EdgeHoleBasedLedExtractor::ContourId contourId = 0;
+                RejectReason reason;
+                cv::Point2d center;
+                std::tie(contourId, reason, center) = reject;
+
+                if (osvr::vbtracker::RejectReason::Area == reason ||
+                    RejectReason::CenterPointValue == reason) {
+                    // Skip drawing these, they clutter the display
+                    continue;
+                }
+                drawSubpixelPoint(highlightedContours, center,
+                                  cv::Scalar(0, 0, 0), 1.2);
+                std::ostringstream os;
+                // os << "[";
+                os << contourId;
+                switch (reason) {
+                case RejectReason::Area:
+                    os << ":Area";
+                    break;
+                case RejectReason::CenterPointValue:
+                    os << ":Value";
+                    break;
+                case RejectReason::Circularity:
+                    os << ":Circ";
+                    break;
+                case RejectReason::Convexity:
+                    os << ":Conv";
+                    break;
+                default:
+                    break;
+                }
+                // os << "]";
+                auto text = boost::algorithm::to_upper_copy(os.str());
+                cv::putText(highlightedContours, text,
+                            computeTextLocation(center, text), FONT_FACE,
+                            FONT_SCALE, cv::Scalar(0, 0, 255), FONT_THICKNESS);
+            }
         }
 
         showImage("Selected contours", highlightedContours, showImages);
