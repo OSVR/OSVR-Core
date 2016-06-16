07397f8ee6f127df3cfe6676d77bbd0025b43730
Ryan Pavlik <ryan@sensics.com>
2016-06-16 10:21:53 -0500

    Revert "Allow room calibration to proceed if only angular velocity is in bounds."
    This reverts commit d4869ae7208391fd3709bdf41911d482ffe1216e.

--- plugins/unifiedvideoinertialtracker/RoomCalibration.cpp
+++ plugins/unifiedvideoinertialtracker/RoomCalibration.cpp
@@ -135,7 +135,9 @@ namespace vbtracker {
         auto linearVel = m_cameraFilter.getLinearVelocityMagnitude();
         auto angVel = m_cameraFilter.getAngularVelocityMagnitude();
 
-        if (angVel < ANGULAR_VELOCITY_CUTOFF) {
+        // std::cout << "linear " << linearVel << " ang " << angVel << "\n";
+        if (linearVel < LINEAR_VELOCITY_CUTOFF &&
+            angVel < ANGULAR_VELOCITY_CUTOFF) {
             // OK, velocity within bounds
             if (m_steadyVideoReports == 0) {
                 msg() << "Hold still, performing room calibration";
