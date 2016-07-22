ac25e42c6505b57abbb14480607dabfc65811540
Kevin M. Godby <kevin@godby.org>
2016-07-22 13:23:39 -0500

    Bump compatible Boost version up to 1.61.0. No interprocess API changes.

--- src/osvr/Common/IPCRingBuffer.cpp
+++ src/osvr/Common/IPCRingBuffer.cpp
@@ -53,7 +53,7 @@ namespace common {
 /// number.
 /// The base boost version test has been moved exclusively to CMake, to error
 /// out earlier.
-#if (BOOST_VERSION > 106000)
+#if (BOOST_VERSION > 106100)
 #error                                                                         \
     "Using an untested Boost version - inspect the Boost Interprocess release notes/changelog to see if any ABI breaks affect us."
 #endif
