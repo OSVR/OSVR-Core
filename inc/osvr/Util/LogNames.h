2f1f7ce85cc1677fc68b3dc9edaacc216c59cb6d
Ryan Pavlik <ryan@sensics.com>
2016-07-26 08:37:48 -0500

    Add a header for log names used in more than one file.

---
+++ inc/osvr/Util/LogNames.h
@@ -0,0 +1,47 @@
+/** @file
+    @brief Header
+
+    @date 2016
+
+    @author
+    Sensics, Inc.
+    <http://sensics.com/osvr>
+*/
+
+// Copyright 2016 Sensics, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef INCLUDED_LogNames_h_GUID_A9468387_BB38_4C9D_EE44_7E98AEEA4115
+#define INCLUDED_LogNames_h_GUID_A9468387_BB38_4C9D_EE44_7E98AEEA4115
+
+// Internal Includes
+// - none
+
+// Library/third-party includes
+// - none
+
+// Standard includes
+// - none
+
+namespace osvr {
+namespace util {
+    namespace log {
+
+        static const auto OSVR_SERVER_LOG = "OSVR Server";
+
+    } // end namespace log
+} // end namespace util
+} // end namespace osvr
+
+#endif // INCLUDED_LogNames_h_GUID_A9468387_BB38_4C9D_EE44_7E98AEEA4115
