5f36a6b108a415ea2c24cdf835f736bc11132d7e
Ryan Pavlik <ryan@sensics.com>
2016-09-23 12:37:52 -0500

    Enhance/complete app for measuring camera stack latency.

--- plugins/unifiedvideoinertialtracker/MeasureTrackingCameraLatency.cpp
+++ plugins/unifiedvideoinertialtracker/MeasureTrackingCameraLatency.cpp
@@ -29,25 +29,70 @@
 #include <osvr/Util/MiniArgsHandling.h>
 
 // Library/third-party includes
+#include <boost/lexical_cast.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <vrpn_SerialPort.h>
 
 // Standard includes
 #include <chrono>
+#include <fstream>
+#include <functional>
 #include <iostream>
 #include <memory>
+#include <random>
 #include <sstream>
 #include <thread>
 
 using SerialBufType = const unsigned char;
 
+/// Utility function to take in a (typically) string literal and write it,
+/// without its null terminator, to the serial port, optionally repeatedly.
+/// (Size deduced by template)
+template <typename T, std::size_t N>
+inline void writeStringWithoutNullTerminator(vrpn_SerialPort &port, T (&buf)[N],
+                                             std::size_t repetitions = 1) {
+    static_assert(sizeof(T) == 1, "Can only pass character/byte arrays");
+    std::size_t len = N;
+    if ('\0' == buf[len - 1]) {
+        len--;
+    }
+    for (std::size_t i = 0; i < repetitions; ++i) {
+        port.write(reinterpret_cast<SerialBufType *>(&(buf[0])), len);
+    }
+}
+
+/// Utility function to take in a string literal and write it,
+/// without its null terminator, followed by a newline, to the serial port,
+/// optionally repeatedly. (Size deduced by template)
+template <typename T, std::size_t N>
+inline void writeCommand(vrpn_SerialPort &port, T (&buf)[N],
+                         std::size_t repetitions = 1) {
+    for (std::size_t i = 0; i < repetitions; ++i) {
+        writeStringWithoutNullTerminator(port, buf);
+        writeStringWithoutNullTerminator(port, "\n");
+    }
+}
+
+/// Utility function to take in a binary array and write it verbatim to the
+/// serial port, optionally repeatedly. (Size deduced by template)
+template <std::size_t N>
+inline void writeBinaryArray(vrpn_SerialPort &port, SerialBufType (&buf)[N],
+                             std::size_t repetitions = 1) {
+    std::size_t len = N;
+    for (std::size_t i = 0; i < repetitions; ++i) {
+        port.write(reinterpret_cast<SerialBufType *>(&(buf[0])), N);
+    }
+}
+
+/// Abstract base class for ways of controlling an IR LED through a serial port.
 class SerialIRLED {
   public:
+    /// Call to cause an IR LED pulse to be emitted.
     void trigger() { handle_trigger(getPort()); }
     virtual ~SerialIRLED() {}
 
   protected:
-    SerialIRLED(vrpn_SerialPort & port) : port_(std::ref(port)) {}
+    SerialIRLED(vrpn_SerialPort &port) : port_(std::ref(port)) {}
     virtual void handle_trigger(vrpn_SerialPort &port) = 0;
 
     vrpn_SerialPort &getPort() { return port_.get(); }
@@ -55,70 +100,44 @@ class SerialIRLED {
   private:
     std::reference_wrapper<vrpn_SerialPort> port_;
 };
+using SerialIRLEDPtr = std::unique_ptr<SerialIRLED>;
+using SerialLEDControllerFactory =
+    std::function<SerialIRLEDPtr(vrpn_SerialPort &)>;
 
-template <typename T, std::size_t N>
-void writeStringWithoutNullTerminator(vrpn_SerialPort &port, T (&buf)[N],
-                                      std::size_t repetitions = 1) {
-    static_assert(sizeof(T) == 1, "Can only pass character/byte arrays");
-    std::size_t len = N;
-    if ('\0' == buf[len - 1]) {
-        len--;
-    }
-    for (std::size_t i = 0; i < repetitions; ++i) {
-        port.write(reinterpret_cast<const unsigned char *>(&(buf[0])), len);
-    }
-}
-
-template <std::size_t N>
-void writeBinaryArray(vrpn_SerialPort &port, SerialBufType (&buf)[N],
-                      std::size_t repetitions = 1) {
-    std::size_t len = N;
-    for (std::size_t i = 0; i < repetitions; ++i) {
-        port.write(reinterpret_cast<const unsigned char *>(&(buf[0])), N);
-    }
-}
-
+/// A class for an LED connected to an Arduino running a sketch that pulses an
+/// LED on then off on receiving a single character over serial.
 class ArduinoLED : public SerialIRLED {
   public:
-    ArduinoLED(vrpn_SerialPort & port) : SerialIRLED(port) {}
+    static SerialIRLEDPtr make(vrpn_SerialPort &port) {
+        SerialIRLEDPtr ret(new ArduinoLED(port));
+        return ret;
+    }
 
   private:
+    ArduinoLED(vrpn_SerialPort &port) : SerialIRLED(port) {}
     void handle_trigger(vrpn_SerialPort &port) override {
         // write any byte to trigger a pulse
-        const SerialBufType buf[] = "a";
-        // comPort.write(reinterpret_cast<const unsigned char *>(buf), 1);
-        port.write(buf, 1);
+        writeStringWithoutNullTerminator(port, "a");
         port.drain_output_buffer();
     }
 };
 
+/// A class for an LED connected to a Bus Pirate's AUX pin (with resistor),
+/// using the binary bit-bang mode to turn it on then off. Bus pirate should be
+/// in normal "terminal" mode before starting.
 class BusPirateAuxLED : public SerialIRLED {
   public:
-    static const std::uint8_t ONE = 0x01;
-    static const std::uint8_t ZERO = 0x00;
+    static SerialIRLEDPtr make(vrpn_SerialPort &port) {
+        SerialIRLEDPtr ret;
+        try {
+            ret.reset(new BusPirateAuxLED(port));
+        } catch (std::exception &e) {
+            std::cerr << "Error setting up bus pirate: " << e.what()
+                      << std::endl;
+        }
+        return ret;
+    }
 
-	// 01001111: aka 0x4f configure everything except AUX as input.
-	static const std::uint8_t PORT_MODE = 0x4f;
-            // 11000000: aka 0xc0 turn on the power only
-	static const std::uint8_t ONLY_POWER_ON = 0xc0;
-	// 11010000: aka 0xd0 turn on the power and AUX
-	static const std::uint8_t POWER_AND_AUX_ON = 0xd0;
-	// 00001111: aka 0x0f reset bus pirate to terminal mode.
-	static const std::uint8_t RESET_TO_TERMINAL = 0x0f;
-
-    BusPirateAuxLED(vrpn_SerialPort & port) : SerialIRLED(port) {
-        enterBitBangMode(port);
-
-        const std::uint8_t configPortModes[] = {
-            PORT_MODE,
-            ONLY_POWER_ON};
-        writeBinaryArray(port, configPortModes);
-        // wait until everything has been sent.
-        port.drain_output_buffer();
-        // throw away replies.
-        port.flush_input_buffer();
-	
-	}
     virtual ~BusPirateAuxLED() {
         const std::uint8_t resetToTerminal[] = {RESET_TO_TERMINAL};
         writeBinaryArray(getPort(), resetToTerminal);
@@ -126,6 +145,40 @@ class BusPirateAuxLED : public SerialIRLED {
     }
 
   private:
+    BusPirateAuxLED(vrpn_SerialPort &port) : SerialIRLED(port) {
+        bool success = enterBitBangMode(port);
+        if (success) {
+            std::cout << "Bus Pirate successfully entered binary bit-bang mode!"
+                      << std::endl;
+        } else {
+            std::cout << "Couldn't get bus pirate into binary bit-bang mode: "
+                         "check that nothing else is using the port and try "
+                         "resetting it."
+                      << std::endl;
+            throw std::runtime_error("Could not transition bus pirate to "
+                                     "binary bit-bang mode: check that nothing "
+                                     "else is using the port and try resetting "
+                                     "it.");
+        }
+
+        const std::uint8_t configPortModes[] = {PORT_MODE, ONLY_POWER_ON};
+        writeBinaryArray(port, configPortModes);
+        // wait until everything has been sent.
+        port.drain_output_buffer();
+        // throw away replies.
+        port.flush_input_buffer();
+    }
+    static const std::uint8_t ONE = 0x01;
+    static const std::uint8_t ZERO = 0x00;
+
+    // 01001111: aka 0x4f configure everything except AUX as input.
+    static const std::uint8_t PORT_MODE = 0x4f;
+    // 11000000: aka 0xc0 turn on the power only
+    static const std::uint8_t ONLY_POWER_ON = 0xc0;
+    // 11010000: aka 0xd0 turn on the power and AUX
+    static const std::uint8_t POWER_AND_AUX_ON = 0xd0;
+    // 00001111: aka 0x0f reset bus pirate to terminal mode.
+    static const std::uint8_t RESET_TO_TERMINAL = 0x0f;
     bool enterBitBangMode(vrpn_SerialPort &port) {
 
         SerialBufType enter[] = "\n";
@@ -134,7 +187,7 @@ class BusPirateAuxLED : public SerialIRLED {
         // write at least 10 enters to get out of any menus.
         writeStringWithoutNullTerminator(port, enter, 10);
         // send reset command
-        writeStringWithoutNullTerminator(port, "#\n");
+        writeCommand(port, "#");
         // wait until everything has been sent.
         port.drain_output_buffer();
         std::cout << "Resetting bus pirate" << std::endl;
@@ -184,12 +237,75 @@ class BusPirateAuxLED : public SerialIRLED {
 
 static const std::string windowNameAndInstructions(
     "OSVR tracking camera preview | q or esc to quit");
+static const auto BUS_PIRATE_ARGS = {"--bp", "--buspirate", "-b"};
+static const auto ARDUINO_ARGS = {"--arduino", "-a"};
+static const auto DEVICE_ARG = "--dev";
+static const auto DEFAULT_DEVICE = "COM1";
+static const auto RATE_ARG = "--rate";
+static const auto DEFAULT_RATE = 115200;
+static const auto MEASUREMENTS_ARG = "--measurements";
+static const auto DEFAULT_MEASUREMENTS = 20;
+
+static const double CUTOFF_VALUE = 200.;
+
+struct Config {
+    SerialLEDControllerFactory controllerFactory;
+    std::string device = DEFAULT_DEVICE;
+    std::uint32_t rate = DEFAULT_RATE;
+    std::uint32_t measurements = DEFAULT_MEASUREMENTS;
+} g_config;
 
 int main(int argc, char *argv[]) {
-    std::string devName;
     {
+        auto withUsageError = [] {
+            /// @todo add usage
+            return 1;
+        };
         using namespace osvr::util::args;
         auto args = makeArgList(argc, argv);
+        if (handle_has_any_iswitch_of(args, BUS_PIRATE_ARGS)) {
+            std::cout << "Will use the Bus Pirate, IR LED on AUX, controller."
+                      << std::endl;
+            g_config.controllerFactory = BusPirateAuxLED::make;
+        }
+        if (handle_has_any_iswitch_of(args, ARDUINO_ARGS)) {
+            if (g_config.controllerFactory) {
+                std::cerr << "Got an arduino arg, but already was specified to "
+                             "use bus pirate!"
+                          << std::endl;
+                return withUsageError();
+            }
+            g_config.controllerFactory = ArduinoLED::make;
+        }
+
+        if (!g_config.controllerFactory) {
+            std::cerr << "No controller type specified! Assuming bus pirate."
+                      << std::endl;
+            g_config.controllerFactory = BusPirateAuxLED::make;
+        }
+
+        handle_value_arg(
+            args, [](std::string const &a) { return a == DEVICE_ARG; },
+            [&](std::string const &val) {
+                std::cout << "Setting serial device to " << val << std::endl;
+                g_config.device = val;
+            });
+
+        handle_value_arg(
+            args, [](std::string const &a) { return a == RATE_ARG; },
+            [&](std::string const &val) {
+                g_config.rate = boost::lexical_cast<std::uint32_t>(val);
+                std::cout << "Setting serial rate to " << g_config.rate
+                          << std::endl;
+            });
+
+        handle_value_arg(
+            args, [](std::string const &a) { return a == MEASUREMENTS_ARG; },
+            [&](std::string const &val) {
+                g_config.measurements = boost::lexical_cast<std::uint32_t>(val);
+                std::cout << "Setting number of measurements to "
+                          << g_config.measurements << std::endl;
+            });
     }
 #ifdef _WIN32
     auto cam = osvr::vbtracker::openHDKCameraDirectShow();
@@ -218,63 +334,99 @@ int main(int argc, char *argv[]) {
     auto frame = cv::Mat{};
     auto grayFrame = cv::Mat{};
 
-    vrpn_SerialPort comPort("COM1", 115200);
+    vrpn_SerialPort comPort(g_config.device.c_str(), g_config.rate);
     if (!comPort.is_open()) {
         std::cerr << "Couldn't open com port!" << std::endl;
         return -1;
     }
-	
-    std::unique_ptr<SerialIRLED> LEDController(new BusPirateAuxLED(comPort));
 
-    bool startedTrigger = false;
+    std::unique_ptr<SerialIRLED> LEDController(
+        g_config.controllerFactory(comPort));
+
+    if (!LEDController) {
+        std::cerr << "Could not start LEDController, exiting." << std::endl;
+        return 1;
+    }
+
+    enum class State { StartedTrigger, SawFlash, AwaitingNewTrigger };
+
     using osvr::util::time::TimeValue;
     TimeValue triggerTime = {};
     double peakVal = 0.;
-    cv::namedWindow(windowNameAndInstructions);
-    auto frameCount = std::size_t{0};
+
+    State s = State::AwaitingNewTrigger;
+
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    // distribution of how many frames we'll wait before triggering a new pulse.
+    std::uniform_int_distribution<int> dist(5, 50);
+    auto generateNewCountdown = [&mt, &dist] { return dist(mt); };
+    int countdownToTrigger = generateNewCountdown();
+
+    std::uint32_t samples = 0;
+    std::ofstream os("latency.csv");
     do {
         TimeValue tv;
         cam->retrieve(frame, grayFrame, tv);
-
-        if (startedTrigger) {
+        switch (s) {
+        case State::AwaitingNewTrigger: {
+            if (countdownToTrigger == 0) {
+                // trigger timing
+                s = State::StartedTrigger;
+                peakVal = 0.;
+                LEDController->trigger();
+                triggerTime = osvr::util::time::getNow();
+            } else {
+                countdownToTrigger--;
+            }
+            break;
+        }
+        case State::StartedTrigger: {
             auto now = osvr::util::time::getNow();
             double minVal, maxVal;
             cv::minMaxIdx(grayFrame, &minVal, &maxVal);
             // if (maxVal < peakVal) {
-            if (maxVal > 150.) {
-                // we got it
-                startedTrigger = false;
+            if (maxVal > CUTOFF_VALUE) {
                 osvrTimeValueDifference(&now, &triggerTime);
                 osvrTimeValueDifference(&tv, &triggerTime);
-                std::cout << /* "Peak value: " << peakVal <<*/ "   Current: "
-                          << maxVal << "\n";
-                std::cout << "Latency from trigger to sample time: "
-                          << tv.microseconds << "us\n";
-                std::cout << "Latency from trigger to retrieval time: "
-                          << now.microseconds << "us\n";
-                cv::imwrite("triggered.png", frame);
-            } /* else {
-                 peakVal = maxVal;
-             }*/
-        }
-
-        ++frameCount;
-        if (frameCount % FRAME_DISPLAY_STRIDE == 0) {
-            frameCount = 0;
-            cv::imshow(windowNameAndInstructions, frame);
-            char key = static_cast<char>(cv::waitKey(1)); // wait 1 ms for a key
-            if ('q' == key || 'Q' == key || 27 /*esc*/ == key) {
-                break;
-            } else if ('t' == key) {
-                // trigger timing
-                startedTrigger = true;
-                peakVal = 0.;
-                auto buf = "a";
-                comPort.write(reinterpret_cast<const unsigned char *>(buf), 1);
-                comPort.drain_output_buffer();
-                triggerTime = osvr::util::time::getNow();
+                if (tv.microseconds > 0) {
+                    // we got it
+                    s = State::SawFlash;
+                    std::cout
+                        << "   Current: "
+                        << maxVal << "\n";
+                    std::cout << "Latency from trigger to sample time: "
+                              << tv.microseconds << "us\n";
+                    std::cout << "Latency from trigger to retrieval time: "
+                              << now.microseconds << "us\n";
+                    cv::imwrite("triggered.png", frame);
+                    samples++;
+					os << tv.microseconds << std::endl;
+                } else {
+                    std::cout << "Got a spurious flash, negative trigger to "
+                                 "sample duration"
+                              << std::endl;
+                }
             }
+            break;
         }
-    } while (cam->grab());
+        case State::SawFlash: {
+            // wait for the flash to disappear
+            double minVal, maxVal;
+            cv::minMaxIdx(grayFrame, &minVal, &maxVal);
+            if (maxVal < CUTOFF_VALUE) {
+                // OK, ready to go for a new trigger at some point.
+                s = State::AwaitingNewTrigger;
+                countdownToTrigger = generateNewCountdown();
+                std::cout << "Will wait " << countdownToTrigger
+                          << " frames before triggering again\n";
+            }
+            break;
+        }
+        }
+    } while (cam->grab() && samples < g_config.measurements);
+
+    LEDController.reset();
+
     return 0;
 }
