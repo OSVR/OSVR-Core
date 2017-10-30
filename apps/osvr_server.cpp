593a3240a19049c3c09f71ec3e19db6d320c9044
Ryan Pavlik <ryan@sensics.com>
2017-10-30 11:17:09 -0500

    Improve formatting of options.

--- apps/osvr_server.cpp
+++ apps/osvr_server.cpp
@@ -60,11 +60,13 @@ int main(int argc, char *argv[]) {
     opt::positional_options_description optionsPositional;
 
     optionsPositional.add("config", -1);
+    // clang-format off
     optionsVisible.add_options()
-        ("config", opt::value< std::vector<std::string> >(), "server configuration filename")
+        ("config", opt::value<std::vector<std::string> >(), "server configuration filename")
         ("help", "display this help message")
         ("verbose,v", "enable verbose logging")
         ("debug,d", "enable debug logging");
+    // clang-format on
     optionsAll.add(optionsVisible);
 
     opt::variables_map values;
